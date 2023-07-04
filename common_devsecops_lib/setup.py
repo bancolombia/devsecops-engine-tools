b0a51a8030dba3f7163fde4658fe5f5031b669da
Rene jalvin <bjalvin@bancolombia.com>
2023-07-04 09:55:21 -0500

    integration sonar and artifactory

--- common_devsecops_lib/setup.py
+++ common_devsecops_lib/setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
 def get_readme():
@@ -11,9 +11,9 @@ def get_requirements():
         return fh.read()
 
 
-setuptools.setup(
+setup(
     name="devsecops_engine_utilities",
-    version="0.0.1",
+    version="0.0.5",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="Common utilities tools for engine devsecops",
@@ -23,22 +23,29 @@ setuptools.setup(
     package_dir={
         "devsecops_engine_utilities": "devsecops_engine_utilities",
     },
-    packages=[
-        "devsecops_engine_utilities",
-        "devsecops_engine_utilities.azuredevops",
-        "devsecops_engine_utilities.azuredevops.infrastructure",
-        "devsecops_engine_utilities.azuredevops.models",
-        "devsecops_engine_utilities.github",
-        "devsecops_engine_utilities.github.infrastructure",
-        "devsecops_engine_utilities.github.models",
-        "devsecops_engine_utilities.input_validations",
-        "devsecops_engine_utilities.utils",
-    ],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=get_requirements(),
+    install_requires=[
+        "multipledispatch==0.6.0",
+        "pyfiglet==0.7",
+        "setuptools==67.6.0",
+        "wheel==0.40.0",
+        "pipenv==2023.3.20",
+        "pytest==7.3.1",
+        "pytest-azurepipelines==1.0.3",
+        "pytest-cov==4.1.0",
+        "coverage==7.2.7",
+        "requests==2.31.0",
+        "mock==5.0.2",
+        "marshmallow==3.19.0",
+        "requests-toolbelt==1.0.0",
+        "isodate==0.6.1",
+        "pytz==2023.3",
+        "python-dateutil==2.8.2",
+    ],
     python_requires=">=3.8",
 )
