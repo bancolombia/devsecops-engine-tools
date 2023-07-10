de1a0362d9d3f0b7af8ce85dec1bbaa9a8c8ef5d
Rene jalvin <bjalvin@bancolombia.com>
2023-07-10 11:56:54 -0500

    delete file not used settigns

--- common_devsecops_lib/devsecops_engine_utilities/defect_dojo/infraestructure/repository/settings.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-
-@dataclasses.dataclass
-class SettingRepo:
-    personal_access_token: str = ""
-    remote_config_repo: str = ""
-    remote_config_path: str = ""
-    system_team_project_id: str = ""
-    organization_url: str = ""
