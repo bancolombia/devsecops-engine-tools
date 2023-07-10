3571f16b4b6fb56b5c8425a61c71fefeea190963
Rene jalvin <bjalvin@bancolombia.com>
2023-07-10 10:40:43 -0500

    delete variables of test

--- common_devsecops_lib/devsecops_engine_utilities/settings.py
+++ common_devsecops_lib/devsecops_engine_utilities/settings.py
@@ -8,14 +8,3 @@ DEVSECOPS_ENGINE_UTILITIES_RUNNER_PLATFORM_RELEASE = platform.release()
 DEVSECOPS_ENGINE_UTILITIES_RUNNER_PYTHON_VERSION = platform.python_version()
 DEVSECOPS_ENGINE_UTILITIES_RUNNER_PYTHON_COMPILER = platform.python_compiler()
 DEVSECOPS_ENGINE_UTILITIES_VERSION = "0.0.1"
-
-# remote
-PERSONAL_ACCESS_TOKEN = "wq2rs7faqtp6xn64m5gezgoxw3ifs5ypomvugrt2v4xglveq3shq"
-REMOTE_CONFIG_REPO = "https://grupobancolombia.visualstudio.com/Vicepresidencia%20Servicios%20de%20Tecnolog%C3%ADa/_git/NU0429001_DevSecOps_Remote_Config"
-REMOTE_CONFIG_PATH = "https://grupobancolombia.visualstudio.com/Vicepresidencia%20Servicios%20de%20Tecnolog%C3%ADa/_git/NU0429001_DevSecOps_Remote_Config"
-print(REMOTE_CONFIG_PATH)
-SYSTEM_TEAM_PROJECT_ID = ""
-# ORGANIZATION_URL = "https://dev.azure.com/PNFEngineTest/"
-ORGANIZATION_URL = "https://grupobancolombia.visualstudio.com/Vicepresidencia%20Servicios%20de%20Tecnolog%C3%ADa/"
-        
-print(ORGANIZATION_URL)
