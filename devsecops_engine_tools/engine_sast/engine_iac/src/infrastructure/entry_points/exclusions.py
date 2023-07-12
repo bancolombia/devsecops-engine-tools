20c2692cf4c013e84e894c3aa8b5eacbb736febf
Octavio Velez Gaviria <ocvelez@bancolombia.com.co>
2023-07-12 10:56:56 -0500

    se ajusta el archivo de exclusiones según modificaciones sugeridas por Cesarillo y se envia el objeto rules desde el entrypoint de engine iac

--- devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/entry_points/exclusions.py
+++ devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/entry_points/exclusions.py
@@ -1,70 +1,56 @@
 exclusion = """{
 	"All": {
         "SCA": {
-            "Exclusions": [
-                {
-                    "check_id": "XRAY-521541",
+			"XRAY-521541" : {
                     "Cve_Id": "CVE-2023-29405",
                     "Create_Date" : "10112023",
                     "Expired_Date": "18112023",
                     "severity": "HIGH",
                     "HU": "4338704"
-                }
-            ]
+            }
         },
         "CHECKOV": {
-            "Exclusions": [
-                {
-                    "check_id": "XRAY-521541",
-                    "Cve_Id": "CVE-2023-29405",
-                    "Create_Date" : "18112023",
-                    "Expired_Date": "undefined",
-                    "severity": "LOW",
-                    "HU": "4338704"
-                },
-                {
-                    "check_id": "CKV2_AWS_123",
-                    "Cve_Id": "N.A",
-                    "Create_Date" : "18112023",
-                    "Expired_Date": "18032024",
-                    "severity": "LOW",
-                    "HU": "4338704"
-                }
-            ]
+			"XRAY-521541" : {
+				"Cve_Id": "CVE-2023-29405",
+				"Create_Date" : "18112023",
+				"Expired_Date": "undefined",
+				"severity": "LOW",
+				"HU": "4338704"
+			},
+			"CKV2_AWS_123" : {
+				"Cve_Id": "N.A",
+				"Create_Date" : "18112023",
+				"Expired_Date": "18032024",
+				"severity": "LOW",
+				"HU": "4338704"
+			}
         }
 	},
 	"AW11111111_ProyectoEjemplo": {
-			"SCA": {
-				"Exclusions": [
-					{
-						"check_id": "XRAY-521541",
-						"Cve_Id": "CVE-2023-29405",
-						"Create_Date" : "10112023",
-						"Expired_Date": "18112023",
-						"severity": "HIGH",
-						"HU": "4338704"
-					}
-				]
-			},
-			"CHECKOV": {
-				"Exclusions": [
-					{
-						"check_id": "XRAY-521541",
-						"Cve_Id": "CVE-2023-29405",
-						"Create_Date" : "18112023",
-						"Expired_Date": "undefined",
-						"severity": "LOW",
-						"HU": "4338704"
-					},
-					{
-						"check_id": "CKV2_AWS_123",
-						"Cve_Id": "N.A",
-						"Create_Date" : "18112023",
-						"Expired_Date": "18032024",
-						"severity": "LOW",
-						"HU": "4338704"
-					}
-				]
+		"SCA": {
+			"XRAY-521541" : {
+				"Cve_Id": "CVE-2023-29405",
+				"Create_Date" : "10112023",
+				"Expired_Date": "18112023",
+				"severity": "HIGH",
+				"HU": "4338704"
 			}
+		},
+		"CHECKOV": {
+			"XRAY-521541" : {
+				"Cve_Id": "CVE-2023-29405",
+				"Create_Date" : "18112023",
+				"Expired_Date": "undefined",
+				"severity": "LOW",
+				"HU": "4338704"
+			},
+			"CKV2_AWS_123" : {
+				"Cve_Id": "N.A",
+				"Create_Date" : "18112023",
+				"Expired_Date": "18032024",
+				"severity": "LOW",
+				"HU": "4338704"
+			}
+		}
 	}
 }"""
