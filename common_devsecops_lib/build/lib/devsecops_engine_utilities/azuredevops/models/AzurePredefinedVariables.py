5c8182a88cd467f732effbc61df75e891831ff10
Octavio Velez Gaviria <ocvelez@bancolombia.com.co>
2023-07-13 17:29:25 -0500

    reestructuración de carpetas para organizar el empaquetado por medio de setup.py feat Cesarillo

---
+++ common_devsecops_lib/build/lib/devsecops_engine_utilities/azuredevops/models/AzurePredefinedVariables.py
@@ -0,0 +1,47 @@
+import os
+from enum import Enum
+from devsecops_engine_utilities.input_validations.env_utils import EnvVariables
+
+""" Info de https://learn.microsoft.com/en-us/azure/devops/pipelines/process/variables?view=azure-devops&tabs=yaml%2Cbatch
+Build variables (DevOps Services) https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml """
+
+
+class EnvVariables:
+    @staticmethod
+    def get_value(env_name):
+        env_var = os.environ.get(env_name)
+        if env_var is None:
+            raise ValueError(
+                f"La variable de entorno {env_name} no está definida")
+        return env_var
+
+
+class BaseEnum(Enum):
+    @property
+    def env_name(self):
+        return self._value_.replace(".", "_").upper()
+
+    def value(self):
+        return EnvVariables.get_value(self.env_name)
+
+
+class SystemVariables(BaseEnum):
+    System_AccessToken = "System.AccessToken"
+    System_CollectionId = "System.CollectionId"
+    System_DefaultWorkingDirectory = "System.DefaultWorkingDirectory"
+    System_StageName = "System.StageName"
+    System_TeamFoundationCollectionUri = "System.TeamFoundationCollectionUri"
+    System_TeamProject = "System.TeamProject"
+    System_TeamProjectId = "System.TeamProject"
+
+
+class BuildVariables(BaseEnum):
+    Build_DefinitionName = "Build.DefinitionName"
+    Build_Repository_Name = "Build.Repository.Name"
+    Build_SourceBranch = "Build.SourceBranch"
+    Build_SourceBranchName = "Build.SourceBranchName"
+    Build_StagingDirectory = "Build.StagingDirectory"
+
+
+class ReleaseVariables(BaseEnum):
+    Release_Definitionname = "Release.DefinitionName"
---
+++ common_devsecops_lib/build/lib/devsecops_engine_utilities/defect_dojo/infraestructure/driver_adapters/product_type.py
@@ -0,0 +1,52 @@
+import json
+from devsecops_engine_utilities.utils.logger_info import MyLogger
+from devsecops_engine_utilities.utils.validation_error import ValidationError
+from devsecops_engine_utilities.defect_dojo.domain.request_objects.import_scan import ImportScanRequest
+from devsecops_engine_utilities.defect_dojo.domain.models.product_type_list import ProductTypeList
+from devsecops_engine_utilities.defect_dojo.domain.models.product_type import ProductType
+from devsecops_engine_utilities.defect_dojo.infraestructure.driver_adapters.settings.settings import VERIFY_CERTIFICATE
+from devsecops_engine_utilities.utils.session_manager import SessionManager
+
+logger = MyLogger.__call__().get_logger()
+
+
+class ProductTypeRestConsumer:
+    def __init__(self, request: ImportScanRequest, session: SessionManager):
+        self.__token = request.token_defect_dojo
+        self.__host = request.host_defect_dojo
+        self.__session = session
+
+    def post_product_type(self, product_type_name: str) -> ProductType:
+        url = f"{self.__host}/api/v2/product_types/"
+
+        data = json.dumps({"name": product_type_name})
+
+        headers = {"Authorization": f"Token {self.__token}", "Content-Type": "application/json"}
+
+        response = self.__session.post(url, headers=headers, data=data)
+
+        if response.status_code != 201:
+            raise ValidationError(response)
+        product_type_object = ProductType.from_dict(response.json())
+        return product_type_object
+
+    def get_product_types(self, product_type_name: str) -> ProductTypeList:
+        url = f"{self.__host}/api/v2/product_types/?name={product_type_name}"
+        headers = {"Authorization": f"Token {self.__token}"}
+        response = self.__session.get(url, headers=headers, data={}, verify=VERIFY_CERTIFICATE)
+        if response.status_code != 200:
+            raise ValidationError(response)
+        product_type_object = ProductTypeList.from_dict(response.json())
+        return product_type_object
+
+    def get_product_type_id(self, id: int):
+        url = f"{self.__host}/api/v2/product_types/{id}/"
+
+        headers = {"Authorization": f"Token {self.__token}"}
+
+        response = self.__session.get(url, headers=headers, data={}, verify=VERIFY_CERTIFICATE)
+        if response.status_code != 200:
+            raise ValidationError(response)
+        logger.info(response)
+        product_type_object = ProductTypeList.from_dict(response.json())
+        return product_type_object
