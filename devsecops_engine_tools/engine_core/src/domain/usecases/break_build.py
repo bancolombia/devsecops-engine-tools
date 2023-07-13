c1b56753c2dd1b77863caa07e735b18e7e492974
Octavio Velez Gaviria <ocvelez@bancolombia.com.co>
2023-07-13 10:13:12 -0500

    obtener los datos del objeto inputCore

--- devsecops_engine_tools/engine_core/src/domain/usecases/break_build.py
+++ devsecops_engine_tools/engine_core/src/domain/usecases/break_build.py
@@ -11,4 +11,11 @@ class BreakBuild:
 
     def validate_level_compliance(self):
         vulnerability_list = self.deserializer_gateway.get_list_vulnerability()
+        level_compliance = self.input_core.level_compliance_defined
+        exclusions = self.input_core.totalized_exclusions
+        rules_scaned = self.input_core.rules_scaned
 
+        if len(vulnerability_list) != 0:
+            print()
+        else:
+            return len(vulnerability_list)
