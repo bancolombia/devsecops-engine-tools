16579769d43333cdf931d2c083d7ec6aeeee6b5f
Octavio Velez Gaviria <ocvelez@bancolombia.com.co>
2023-07-13 15:41:14 -0500

    se crea el caso de uso para romper el build y se utiiza programacion funcional para dejar todo condensado según recomendaciones de Cesarillo

--- devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/entry_points/exclusions.py
+++ devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/entry_points/exclusions.py
@@ -23,6 +23,13 @@ exclusion = """{
 				"Expired_Date": "18032024",
 				"severity": "LOW",
 				"HU": "4338704"
+			},
+            "CKV_K8S_37" : {
+				"Cve_Id": "N.A",
+				"Create_Date" : "18112023",
+				"Expired_Date": "18032024",
+				"severity": "LOW",
+				"HU": "4338704"
 			}
         }
 	},
@@ -45,6 +52,13 @@ exclusion = """{
 				"HU": "4338704"
 			},
 			"CKV2_AWS_123" : {
+				"Cve_Id": "N.A",
+				"Create_Date" : "18112023",
+				"Expired_Date": "18032024",
+				"severity": "LOW",
+				"HU": "4338704"
+			},
+			"CKV_DOCKER_4" : {
 				"Cve_Id": "N.A",
 				"Create_Date" : "18112023",
 				"Expired_Date": "18032024",
