87aa2cb08563937703efc6a14a7b04a3114e6296
Rene jalvin <bjalvin@bancolombia.com>
2023-07-14 08:23:34 -0500

    import scan sucessfull

--- common_devsecops_lib/devsecops_engine_utilities/defect_dojo/infraestructure/driver_adapters/scan_configurations.py
+++ common_devsecops_lib/devsecops_engine_utilities/defect_dojo/infraestructure/driver_adapters/scan_configurations.py
@@ -2,6 +2,10 @@ import json
 from devsecops_engine_utilities.utils.logger_info import MyLogger
 from devsecops_engine_utilities.utils.validation_error import ValidationError
 from devsecops_engine_utilities.defect_dojo.domain.request_objects.import_scan import ImportScanRequest
+from devsecops_engine_utilities.defect_dojo.domain.models.scan_configuration import (
+    ScanConfiguration,
+    ScanConfigurationList,
+)
 from devsecops_engine_utilities.defect_dojo.domain.models.scan_configuration import ScanConfiguration
 from devsecops_engine_utilities.defect_dojo.infraestructure.driver_adapters.settings.settings import VERIFY_CERTIFICATE
 from devsecops_engine_utilities.utils.session_manager import SessionManager
@@ -10,12 +14,14 @@ logger = MyLogger.__call__().get_logger()
 
 
 class ScanConfigrationRestConsumer:
-    def __init__(self, request: ImportScanRequest, session: SessionManager):
+    def __init__(self, request: ImportScanRequest, session: SessionManager) -> ScanConfiguration:
         self.__token = request.token_defect_dojo
         self.__host = request.host_defect_dojo
         self.__session = session
 
-    def post_api_scan_configuration(self, request: ImportScanRequest, product_id: int, tool_configuration_id: int):
+    def post_api_scan_configuration(
+        self, request: ImportScanRequest, product_id: int, tool_configuration_id: int
+    ) -> ScanConfiguration:
         url = f"{self.__host}/api/v2/product_api_scan_configurations/"
 
         headers = {
@@ -33,6 +39,7 @@ class ScanConfigrationRestConsumer:
 
         response = self.__session.post(url=url, headers=headers, data=data, verify=VERIFY_CERTIFICATE)
         if response.status_code != 201:
+            logger.error(response.json())
             raise ValidationError(response)
         try:
             scan_configuration_object = ScanConfiguration.from_dict(response.json())
@@ -40,3 +47,16 @@ class ScanConfigrationRestConsumer:
             logger.error(f"from dict scanConfiguration {response.json()}")
             ValidationError(e)
         return scan_configuration_object
+
+    def get_api_scan_configuration(self, request: ImportScanRequest) -> ScanConfigurationList:
+        url = f"{self.__host}/api/v2/product_api_scan_configurations/?service_key_1={request.engagement_name}"
+        headers = {
+            "Authorization": f"Token {self.__token}",
+            "Conten-Type": "application/json",
+        }
+        response = self.__session.get(url=url, headers=headers, verify=VERIFY_CERTIFICATE)
+        if response.status_code != 200:
+            logger.error(response.json())
+            raise ValidationError(response)
+        response = ScanConfigurationList.from_dict(response.json())
+        return response
