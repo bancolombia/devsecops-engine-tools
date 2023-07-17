90d6c615f765cff32afeba49d22772c4da5f43a4
Rene jalvin <bjalvin@bancolombia.com>
2023-07-17 15:11:18 -0500

    update version 2.0.11

--- common_devsecops_lib/setup.py
+++ common_devsecops_lib/setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name="devsecops_engine_utilities",
-    version="2.0.9",
+    version="2.0.11",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="Common utilities tools for engine devsecops",
