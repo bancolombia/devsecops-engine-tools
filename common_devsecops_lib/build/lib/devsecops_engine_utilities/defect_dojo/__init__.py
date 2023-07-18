d20766c98714f31ac7a7149e9671220e372e877e
Octavio Velez Gaviria <ocvelez@bancolombia.com.co>
2023-07-18 11:26:48 -0500

    eliminar carpeta build que se subio por error

--- common_devsecops_lib/build/lib/devsecops_engine_utilities/defect_dojo/__init__.py
+++
@@ -1,3 +0,0 @@
-from .domain.request_objects.import_scan import ImportScanRequest
-from .applications.defect_dojo import DefectDojo
-from .applications.connect import Connect
--- common_devsecops_lib/build/lib/devsecops_engine_utilities/utils/printers.py
+++
@@ -1,7 +0,0 @@
-import pyfiglet
-
-
-class Printers:
-    @staticmethod
-    def print_logo_tool():
-        print(pyfiglet.figlet_format("DevSecOps Bancolombia", font="slant"))
