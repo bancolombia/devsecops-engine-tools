3713caf11c263c335c615487828bc4ab4eccc381
Rene jalvin <bjalvin@bancolombia.com>
2023-07-28 14:28:36 -0500

    soluction engagement status

--- common_devsecops_lib/setup.py
+++ common_devsecops_lib/setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name="devsecops_engine_utilities",
-    version="2.1.13",
+    version="2.1.15",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="Common utilities tools for engine devsecops",
