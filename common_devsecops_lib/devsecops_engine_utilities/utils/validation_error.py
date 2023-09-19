621bc5e0414bdaec42513b51b3f6ef5be0d37cf0
Rene jalvin <bjalvin@bancolombia.com>
2023-09-19 00:30:21 -0500

    refactoring test

--- common_devsecops_lib/devsecops_engine_utilities/utils/validation_error.py
+++ common_devsecops_lib/devsecops_engine_utilities/utils/validation_error.py
@@ -1,10 +1,8 @@
 from devsecops_engine_utilities.utils.logger_info import MyLogger
+from marshmallow import ValidationError
 from requests import Response
 
 
-class ValidationError(Exception):
+class ValidationErrorBase(ValidationError):
     def __init__(self, message):
-        if isinstance(message, Response):
-            super().__init__(message.json())
-        else:
-            super().__init__(message)
+        super().__init__({"error": message})
