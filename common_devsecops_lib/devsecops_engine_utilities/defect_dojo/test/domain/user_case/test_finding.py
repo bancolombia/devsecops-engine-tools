abceb31c58af047a623dcb30d725577c4732d692
Rene jalvin <bjalvin@bancolombia.com>
2023-10-09 15:05:43 -0500

    add timezone for parameter close finding endpoint

--- common_devsecops_lib/devsecops_engine_utilities/defect_dojo/test/domain/user_case/test_finding.py
+++ common_devsecops_lib/devsecops_engine_utilities/defect_dojo/test/domain/user_case/test_finding.py
@@ -1,2 +1,11 @@
-def test_execute():
-    pass
+# from devsecops_engine_utilities.defect_dojo.domain.user_case.finding import FindingUserCase
+# from devsecops_engine_utilities.utils.session_manager import SessionManager
+# from devsecops_engine_utilities.defect_dojo.infraestructure.driver_adapters.finding import FindingRestConsumer
+
+# def test_execute():
+#     session = SessionManager(token="test_token8643f700137f71c15f8980", host="http://localhost:8000/")
+#     rest_consumer = FindingRestConsumer(session)
+# assert rest_consumer.__token == "test_token8643f700137f71c15f8980"
+# assert rest_consumer.__host == "http://localhost:8000/"
+
+# Creation mocks, _rest_finding.get and __rest_finding_close
