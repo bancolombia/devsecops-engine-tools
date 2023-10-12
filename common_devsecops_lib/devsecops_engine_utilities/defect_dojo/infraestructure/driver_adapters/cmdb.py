ec041b932e625b5bfcd4a040b7016fa735b84b1b
Rene jalvin <bjalvin@bancolombia.com>
2023-10-11 22:17:50 -0500

    add try execpt

--- common_devsecops_lib/devsecops_engine_utilities/defect_dojo/infraestructure/driver_adapters/cmdb.py
+++ common_devsecops_lib/devsecops_engine_utilities/defect_dojo/infraestructure/driver_adapters/cmdb.py
@@ -19,21 +19,21 @@ class CmdbRestConsumer:
     def get_product_info(self, code_app: int) -> Cmdb:
         data = json.dumps({"codapp": code_app})
         headers = {"tokenkey": self.__token, "Content-Type": "application/json"}
-
         logger.info("Search info of name product")
-
-        response = self.__session.post(self.__host, headers=headers, data=data, verify=VERIFY_CERTIFICATE)
-
-        if response.status_code != 200:
-            raise ApiError(response.json()["Message"])
-        if response.json() == []:
-            e = f"Engagement: {code_app} not found"
-            logger.error(e)
+        try:
+            response = self.__session.post(self.__host, headers=headers, data=data, verify=VERIFY_CERTIFICATE)
+            if response.status_code != 200:
+                raise ApiError(response.json()["Message"])
+            if response.json() == []:
+                e = f"Engagement: {code_app} not found"
+                logger.error(e)
+                raise ApiError(e)
+            data = response.json()[0]
+            data_map = self.mapping_cmdb(data)
+            logger.info(data_map)
+            cmdb_object = Cmdb.from_dict(data_map)
+        except Exception as e:
             raise ApiError(e)
-        data = response.json()[0]
-        data_map = self.mapping_cmdb(data)
-        logger.info(data_map)
-        cmdb_object = Cmdb.from_dict(data_map)
         return cmdb_object
 
     def mapping_cmdb(self, data):
