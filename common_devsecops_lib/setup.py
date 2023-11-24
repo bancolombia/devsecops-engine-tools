820e51db7c856a1ba302721a7068cc92bda85adc
Rene jalvin <bjalvin@bancolombia.com>
2023-11-24 14:27:42 -0500

    update version 2.4.28

--- common_devsecops_lib/setup.py
+++ common_devsecops_lib/setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name="devsecops_engine_utilities",
-    version="2.4.27",
+    version="2.4.28",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="Common utilities tools for engine devsecops",
