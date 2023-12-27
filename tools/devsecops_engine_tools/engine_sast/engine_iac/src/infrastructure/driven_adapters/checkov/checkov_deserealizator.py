cd562427a6b47d466f81181198c0cb790972f651
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2023-12-27 17:48:40 -0500

    refacotr module engine_iac

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
@@ -1,16 +1,16 @@
-from devsecops_engine_tools.engine_sast.engine_iac.src.domain.model.gateways.gateway_deserealizator import DeseralizatorGateway
-from devsecops_engine_tools.engine_core.src.domain.model.vulnerability import Vulnerability
+from devsecops_engine_tools.engine_core.src.domain.model.vulnerability import (
+    Vulnerability,
+)
 from datetime import datetime
 from dataclasses import dataclass
 
 
 @dataclass
-class CheckovDeserealizator(DeseralizatorGateway):
-
-    def get_list_vulnerability(self, results_scan_list: list, rules) -> "list[Vulnerability]":
-        list_open_vulnerbailities = []
-        # TODO OCVELEZ: Mirar si es posible hacer uso de la librería https://pypi.org/project/attrdict/ para mejorar
-        # la forma de mapear de json a objetos
+class CheckovDeserealizator:
+    def get_list_vulnerability(
+        self, results_scan_list: list, rules
+    ) -> "list[Vulnerability]":
+        list_open_vulnerabilities = []
 
         for result in results_scan_list:
             if "failed_checks" in str(result):
@@ -22,11 +22,11 @@ class CheckovDeserealizator(DeseralizatorGateway):
                         description=scan.get("check_name"),
                         severity=rules[scan.get("check_id")].get("severity").lower(),
                         identification_date=datetime.now().strftime("%d%m%Y"),
-                        type_vulnerability="IaaC",
+                        type_vulnerability="IaC",
                         requirements=scan.get("guideline"),
                         tool="Checkov",
                         is_excluded=False,
                     )
-                    list_open_vulnerbailities.append(vulnerability_open)
+                    list_open_vulnerabilities.append(vulnerability_open)
 
-        return list_open_vulnerbailities
+        return list_open_vulnerabilities
