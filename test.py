bb1cfbd5f85b65fb5c2ad5b942e06230a528f759
rnorena <rnorena@bancolombia.com.co>
2024-01-03 10:17:01 -0500

    adjust test files

--- test.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open('C:\\Users\\rnorena\\Documents\\Trabajo talento b\\Sprints\\Sprint 177\\Engine DAST\\NU0429001_devsecops_engine\\result.json', "r") as file:
-    json_data = json.load(file)
-    print(json_data)
