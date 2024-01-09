f48b2518e038366f12557a4e15b6a878ad8ea19a
Santiago Santa <santsant@bancolombia.com.co>
2024-01-09 14:26:52 -0500

    Adding more unit tests to trivy_manager_scan

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_manager_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_manager_scan.py
@@ -15,12 +15,14 @@ class TrivyScan(ToolGateway):
     def install_trivy(self,version):
         try:
             subprocess.run(['which', 'trivy'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            # print("trivy está instalado.")
+            print("trivy está instalado.")
         except subprocess.CalledProcessError:
-            # print("Intentando instalar trivy.")
+            print("Intentando instalar trivy.")
             try:
-                subprocess.run(['wget', 'https://github.com/aquasecurity/trivy/releases/download/v'+version+'/trivy_'+version+'_Linux-64bit.deb'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                subprocess.run(['sudo', 'dpkg', '-i', 'trivy_'+version+'_Linux-64bit.deb'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                command1 = ['wget', 'https://github.com/aquasecurity/trivy/releases/download/v'+version+'/trivy_'+version+'_Linux-64bit.deb']
+                subprocess.run(command1, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                command2 = ['sudo', 'dpkg', '-i', 'trivy_'+version+'_Linux-64bit.deb']
+                subprocess.run(command2, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 # print("trivy instalado.")
             except subprocess.CalledProcessError as error:
                 raise RuntimeError(f"Error al instalar trivy: {error}")
@@ -42,17 +44,18 @@ class TrivyScan(ToolGateway):
                         print(f"The image {image_name} has already been scan previously.")
                     else:
                         try:
-                            subprocess.run(['trivy', 'image', '--download-db-only'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                            command = ['trivy', '--scanners', 'vuln', '-f', 'json', '-o', image_name+'_scan_result.json', '--quiet', 'image', image_name]
-                            subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                            command1 = ['trivy', 'image', '--download-db-only']
+                            subprocess.run(command1, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            command2 = ['trivy', '--scanners', 'vuln', '-f', 'json', '-o', image_name+'_scan_result.json']
+                            command2.extend(['--quiet', 'image', image_name])
+                            subprocess.run(command2, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                             images_scanned.append(image_name+"_scan_result.json")
                             with open(file_name, 'a') as file:
                                 file.write(image_name+'_scan_result.json\n')
                         except subprocess.CalledProcessError as e:
                             print("Error scanning "+image_name+" image: "+e.stderr)
-            
+
             return images_scanned
-        
+
         except Exception as ex:
             print(f"Could not get Azure Remote Config: {ex}")
-        
