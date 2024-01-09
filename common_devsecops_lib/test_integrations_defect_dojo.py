d48af19688b10717c10812c1541a1dfe563365f9
Rene jalvin <bjalvin@bancolombia.com>
2024-01-09 17:08:42 -0500

    update version

--- common_devsecops_lib/test_integrations_defect_dojo.py
+++ common_devsecops_lib/test_integrations_defect_dojo.py
@@ -63,25 +63,25 @@ if __name__ == "__main__":
     try:
         if settings.INTEGRATION_TEST:
             """test integration Aws security Finding"""
-            # Printers.print_title("AWS Security Finding Format (ASFF) Scan")
-            # response = import_scan(
-            #     scan_type="AWS Security Finding Format (ASFF) Scan", file_path=f"{path_file}/aws_security_finding.json"
-            # )
-            # logger.debug(f"response: {response}")
-            # table.append(validate_response(response, scan_type="AWS Security Hub", end_point="impor_scan"))
+            Printers.print_title("AWS Security Finding Format (ASFF) Scan")
+            response = import_scan(
+                scan_type="AWS Security Finding Format (ASFF) Scan", file_path=f"{path_file}/aws_security_finding.json"
+            )
+            logger.debug(f"response: {response}")
+            table.append(validate_response(response, scan_type="AWS Security Hub", end_point="impor_scan"))
 
-            # Printers.print_title("JFrog Xray On Demand Binary Scan")
-            # response = import_scan(
-            #     scan_type="JFrog Xray On Demand Binary Scan",
-            #     file_path=f"{path_file}/jfrog-xray_on_demand_binary_scan.json",
-            # )
-            # logger.debug(f"response: {response}")
-            # table.append(validate_response(response, scan_type="Jfrog Xray", end_point="impor_scan"))
+            Printers.print_title("JFrog Xray On Demand Binary Scan")
+            response = import_scan(
+                scan_type="JFrog Xray On Demand Binary Scan",
+                file_path=f"{path_file}/jfrog-xray_on_demand_binary_scan.json",
+            )
+            logger.debug(f"response: {response}")
+            table.append(validate_response(response, scan_type="Jfrog Xray", end_point="impor_scan"))
 
-            # # test integration Checkov
-            # Printers.print_title("Checkov Scan")
-            # response = import_scan(scan_type="Checkov Scan", file_path=f"{path_file}/checkov.json")
-            # table.append(validate_response(response, scan_type="Checkov Scan", end_point="impor_scan"))
+            # test integration Checkov
+            Printers.print_title("Checkov Scan")
+            response = import_scan(scan_type="Checkov Scan", file_path=f"{path_file}/checkov.json")
+            table.append(validate_response(response, scan_type="Checkov Scan", end_point="impor_scan"))
 
             """ test integration Twistlock Image Scan Json """
             Printers.print_title("Twistlock Image Scan JSON")
@@ -93,23 +93,23 @@ if __name__ == "__main__":
             response = import_scan(scan_type="Twistlock Image Scan", file_path=f"{path_file}/twistlock.csv")
             table.append(validate_response(response, scan_type="Twistlock Image Scan", end_point="impor_scan csv"))
 
-            # # test SonarQuebe
-            # Printers.print_title("SonarQube API Import")
-            # response = import_scan(scan_type="SonarQube API Import")
-            # logger.debug(f"SonarQube Api Import: {response}")
-            # table.append(validate_response(response, scan_type="SonarQube", end_point="impor_scan"))
+            # test SonarQuebe
+            Printers.print_title("SonarQube API Import")
+            response = import_scan(scan_type="SonarQube API Import")
+            logger.debug(f"SonarQube Api Import: {response}")
+            table.append(validate_response(response, scan_type="SonarQube", end_point="impor_scan"))
 
-            # # test get finding
-            # session = SessionManager(token=settings.TOKEN_DEFECT_DOJO, host=settings.HOST_DEFECT_DOJO)
-            # Printers.print_title("Get Finding")
-            # response = Finding.get_finding(session=session, risk_accepted=True)
-            # logger.debug(f"Finding get {response}")
-            # table.append(validate_response(response, end_point="finding.get"))
+            # test get finding
+            session = SessionManager(token=settings.TOKEN_DEFECT_DOJO, host=settings.HOST_DEFECT_DOJO)
+            Printers.print_title("Get Finding")
+            response = Finding.get_finding(session=session, risk_accepted=True)
+            logger.debug(f"Finding get {response}")
+            table.append(validate_response(response, end_point="finding.get"))
 
-            # # test integration Finding close
-            # Printers.print_title("Finding Close")
-            # response = Finding.close_finding(session, unique_id_from_tool="1")
-            # logger.debug(f"Finding_close: {response}")
+            # test integration Finding close
+            Printers.print_title("Finding Close")
+            response = Finding.close_finding(session, unique_id_from_tool="1")
+            logger.debug(f"Finding_close: {response}")
 
             table.append(validate_response(response, end_point="finding.close"))
             print(tabulate(table, headers=["End_point", "Description", "Status", "Result"]))
