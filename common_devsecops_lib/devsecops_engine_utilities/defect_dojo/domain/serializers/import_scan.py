adc76cc31b5d800804af448f6f806adce044d8fb
Rene jalvin <bjalvin@bancolombia.com>
2024-01-10 17:18:16 -0500

    add doc logger and fromDictMixin

--- common_devsecops_lib/devsecops_engine_utilities/defect_dojo/domain/serializers/import_scan.py
+++ common_devsecops_lib/devsecops_engine_utilities/defect_dojo/domain/serializers/import_scan.py
@@ -79,7 +79,7 @@ list_scan_type = [
     "Immuniweb Scan",
     "IntSights Report",
     "JFrog Xray API Summary Artifact Scan",
-    "Jfrog Xray On Demand Binary Scan",
+    "JFrog Xray On Demand Binary Scan",
     "JFrog Xray Scan",
     "JFrog Xray Unified Scan",
     "KICS Scan",
@@ -196,7 +196,7 @@ class ImportScanSerializer(Schema):
     test_title = fields.Str(required=False)
     description_product = fields.Str(required=False)
     create_finding_groups_for_all_findings = fields.Str(required=False)
-    tools_configuration = fields.Int(required=False)
+    tools_configuration = fields.Int(required=False, load_default=1)
     code_app = fields.Str(required=False)
     # defect-dojo credential
     token_cmdb = fields.Str(required=True)
