17e0f9015c5db1ca816525fabf22ac339824ee20
sarbole <sarbole@bancolombia.com.co>
2024-01-10 09:55:28 -0500

    format

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/config_gateway.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/config_gateway.py
@@ -1,11 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
-
-
-
 class ConfigGateway (metaclass=ABCMeta):
 
     @abstractmethod
     def get_remote_config (self) -> str:
         "get remote config"
-    
