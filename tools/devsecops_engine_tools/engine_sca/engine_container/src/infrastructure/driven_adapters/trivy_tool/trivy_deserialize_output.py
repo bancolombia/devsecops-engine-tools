559356ecba5c70e657c929a5e2d5f62d5a0cfdbd
Santiago Santa <santsant@bancolombia.com.co>
2024-01-10 15:56:26 -0500

    Refactoring code smells

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_deserialize_output.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_deserialize_output.py
@@ -1,40 +1,47 @@
 from devsecops_engine_tools.engine_sca.engine_container.src.domain.model.gateways.deserealizator_gateway import (
-    DeseralizatorGateway
-    )
+    DeseralizatorGateway,
+)
 from devsecops_engine_tools.engine_core.src.domain.model.finding import (
     Finding,
-    Category
-    )
+    Category,
+)
 from dataclasses import dataclass
 import json
 
+
 @dataclass
 class TrivyDeserializator(DeseralizatorGateway):
-
     def get_list_vulnerability(self, images_scanned: list) -> "list[Finding]":
         list_open_vulnerabilities = []
         for image in images_scanned:
             with open(image, "rb") as file:
                 image_object = file.read()
                 json_data = json.loads(image_object)
-                if 'Vulnerabilities' in json_data['Results'][0]:
-                    vulnerabilities_data = json_data["Results"][0]["Vulnerabilities"]
-                    vulnerabilities = []
-                    for vul in vulnerabilities_data:
-                        if 'CVSS' in vul:
-                            vulnerabilities.append(
-                                Finding(
-                                id=vul.get("VulnerabilityID",""),
-                                cvss=next((v["V3Score"] for v in vul["CVSS"].values() if "V3Score" in v), None),
-                                where=vul.get("PkgName", "")+" "+vul.get("InstalledVersion", ""),
-                                description=vul.get("Description", "").replace("\n",""),
-                                severity=vul.get("Severity", "").lower(),
-                                identification_date=vul.get("PublishedDate", ""),
-                                module="CONTAINER",
-                                category=Category.VULNERABILITY,
-                                requirements=vul.get("FixedVersion") if vul.get("FixedVersion") else vul.get("Status", ""),
-                                tool="Trivy",
-                                )
-                            )
-                    list_open_vulnerabilities.extend(vulnerabilities)
+                vulnerabilities_data = json_data["Results"][0]["Vulnerabilities"]
+                vulnerabilities = [
+                    Finding(
+                        id=vul.get("VulnerabilityID", ""),
+                        cvss=next(
+                            (
+                                v["V3Score"]
+                                for v in vul["CVSS"].values()
+                                if "V3Score" in v
+                            ),
+                            None,
+                        ),
+                        where=vul.get("PkgName", "")
+                        + " "
+                        + vul.get("InstalledVersion", ""),
+                        description=vul.get("Description", "").replace("\n", ""),
+                        severity=vul.get("Severity", "").lower(),
+                        identification_date=vul.get("PublishedDate", ""),
+                        module="CONTAINER",
+                        category=Category.VULNERABILITY,
+                        requirements=vul.get("FixedVersion") or vul.get("Status", ""),
+                        tool="Trivy",
+                    )
+                    for vul in vulnerabilities_data
+                    if "CVSS" in vul
+                ]
+                list_open_vulnerabilities.extend(vulnerabilities)
         return list_open_vulnerabilities
