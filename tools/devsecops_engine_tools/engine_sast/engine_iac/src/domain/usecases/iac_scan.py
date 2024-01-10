b343e1d673b8292152900a79ab7772ee4241d0c5
cjguerre <cjguerre@bancolombia.com.co>
2024-01-10 13:51:01 -0500

    openshift podtopology exception handler

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/domain/usecases/iac_scan.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/domain/usecases/iac_scan.py
@@ -26,6 +26,7 @@ class IacScan:
             init_config_tool,
             exclusions,
             dict_args["environment"],
+            dict_args["platform"],
             self.devops_platform_gateway.get_variable("pipeline"),
             secret_tool,
         )
