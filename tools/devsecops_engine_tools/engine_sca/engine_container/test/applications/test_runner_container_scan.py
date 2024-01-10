5cbe0893185d8b78242133e2c2aa38c47c5b4015
Santiago Santa <santsant@bancolombia.com.co>
2024-01-10 11:58:12 -0500

    Refactor test_runner_container_scan

--- tools/devsecops_engine_tools/engine_sca/engine_container/test/applications/test_runner_container_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/test/applications/test_runner_container_scan.py
@@ -31,7 +31,11 @@ def test_runner_engine_container_with_prisma():
         'config_tool': {'ENGINE_CONTAINER': 'prisma'},
         'token': 'token'
     }
-    with patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.entry_points.entry_point_tool.init_engine_sca_rm', return_value='result'):
+    with patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.prisma_cloud.prisma_cloud_manager_scan.PrismaCloudManagerScan', return_value=MockPrismaCloudManagerScan()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.prisma_cloud.prisma_deserialize_output.PrismaDeserealizator', return_value=MockPrismaDeserealizator()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.docker.docker_images.DockerImages', return_value=MockDockerImages()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.azure.azure_remote_config.AzureRemoteConfig', return_value=MockAzureRemoteConfig()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.entry_points.entry_point_tool.init_engine_sca_rm', return_value='result'):
         with pytest.raises(Exception):
             runner_engine_container(**valid_args_and_config)
 
@@ -44,6 +48,10 @@ def test_runner_engine_container_with_trivy():
         'config_tool': {'ENGINE_CONTAINER': 'trivy'},
         'token': 'token'
     }
-    with patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.entry_points.entry_point_tool.init_engine_sca_rm', return_value='result'):
+    with patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.trivy_tool.trivy_manager_scan.TrivyScan', return_value=MockTrivyScan()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.trivy_tool.trivy_deserialize_output.TrivyDeserializator', return_value=MockTrivyDeserializator()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.docker.docker_images.DockerImages', return_value=MockDockerImages()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.azure.azure_remote_config.AzureRemoteConfig', return_value=MockAzureRemoteConfig()), \
+         patch('devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.entry_points.entry_point_tool.init_engine_sca_rm', return_value='result'):
         with pytest.raises(Exception):
             runner_engine_container(**valid_args_and_config)
