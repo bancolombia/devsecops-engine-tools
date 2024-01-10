af4eac324cf28c771b0d0603416bda30a91683a3
sarbole <sarbole@bancolombia.com.co>
2024-01-10 12:29:21 -0500

    format

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/config_gateway.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/config_gateway.py
@@ -3,8 +3,9 @@ from abc import ABCMeta, abstractmethod
 
 class ConfigGateway(metaclass=ABCMeta):
     @abstractmethod
-    def get_remote_config(self,dict_args) -> str:
+    def get_remote_config(self, dict_args) -> str:
         "get remote config"
+
     @abstractmethod
-    def get_variable(self,variable) -> str:
+    def get_variable(self, variable) -> str:
         "get variable pipeline"
