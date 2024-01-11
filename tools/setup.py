2bff6686310744b7d8809c5b420d88ec10619efa
Santiago Santa <santsant@bancolombia.com.co>
2024-01-11 15:20:25 -0500

    Update tool setup

--- tools/setup.py
+++ tools/setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name="devsecops_engine_tools",
-    version="1.6.12",
+    version="1.5.2",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="tool for devsecops strategy",
