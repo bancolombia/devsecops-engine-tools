c23102ca3b0ab496783f733fe595de08ee017f57
sarbole <sarbole@bancolombia.com.co>
2024-01-11 14:01:23 -0500

    comments

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/images_gateway.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/model/gateways/images_gateway.py
@@ -4,4 +4,4 @@ from abc import ABCMeta, abstractmethod
 class ImagesGateway(metaclass=ABCMeta):
     @abstractmethod
     def list_images(self) -> str:
-        "list images docker"
+        "list images"
