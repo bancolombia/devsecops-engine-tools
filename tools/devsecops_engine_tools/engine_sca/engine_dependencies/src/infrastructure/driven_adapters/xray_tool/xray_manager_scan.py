672bd1bfb41ee088af7f5effac361dc00b3166f0
Santiago Santa <santsant@bancolombia.com.co>
2024-01-18 16:41:37 -0500

    Generating file with scan results

--- tools/devsecops_engine_tools/engine_sca/engine_dependencies/src/infrastructure/driven_adapters/xray_tool/xray_manager_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_dependencies/src/infrastructure/driven_adapters/xray_tool/xray_manager_scan.py
@@ -6,6 +6,8 @@ import subprocess
 import platform
 import requests
 import re
+import os
+import json
 
 from devsecops_engine_utilities.utils.logger_info import MyLogger
 from devsecops_engine_utilities import settings
@@ -37,7 +39,7 @@ class XrayScan(ToolGateway):
                         command2, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
                     )
             except subprocess.CalledProcessError as error:
-                raise RuntimeError(f"Error al instalar Jfrog Cli en Linux: {error}")
+                logger.error(f"Error al instalar Jfrog Cli en Linux: {error}")
         
 
     def install_tool_windows(self, version):
@@ -55,38 +57,57 @@ class XrayScan(ToolGateway):
                 with open(exe_file, 'wb') as archivo:
                     archivo.write(response.content)
             except subprocess.CalledProcessError as error:
-                raise RuntimeError(f"Error al instalar Jfrog Cli en Windows: {error}")
+                logger.error(f"Error al instalar Jfrog Cli en Windows: {error}")
 
     def config_server(self, prefix, token):
-        command = [
-            prefix,
-            "c",
-            "im",
-            token
-        ]
-        result = subprocess.run(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        )
-        if result.returncode == 0:
+        try:
+            c_import = [
+                prefix,
+                "c",
+                "im",
+                token
+            ]
+            result = subprocess.run(
+                c_import, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
             server_id = re.search(r"'(.*?)'", result.stderr).group(1)
-
-            print(result.stderr)
-        else:
-            raise RuntimeError(f"Error al importar artifactory server: {result.stderr}")
-        return 0
+            c_set_server = [
+                prefix,
+                "c",
+                "use",
+                server_id
+            ]
+            subprocess.run(
+                c_set_server, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
+        except subprocess.CalledProcessError as error:
+            logger.error(f"Error al configurar xray server: {error}")
 
     def scan_dependencies(self, prefix):
+        if os.path.isdir("build"):
+            directory_name = "./build/"
+        elif os.path.isdir("target"):
+            directory_name = "./target/"
+        else:
+            logger.error(f"Error al escanear dependencias: No existe el directorio build o target en la ruta {os.getcwd()}")
+            return None
         try:
             command = [
                 prefix,
                 "scan",
+                "--format=simple-json",
+                directory_name
             ]
             result = subprocess.run(
-                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
             )
-        except:
-            return 0
-        return 0
+            scan_result = json.loads(result.stdout)
+            file_result = 'scan_result.json'
+            with open(file_result, 'w') as file:
+                json.dump(scan_result, file, indent=4)
+            return file_result
+        except subprocess.CalledProcessError as error:
+            logger.error(f"Error al escanear dependencias: {error}")
 
     def run_tool_dependencies_sca(self, remote_config, token):
         cli_version = remote_config["JFROG"]["CLI_VERSION"]
@@ -98,4 +119,5 @@ class XrayScan(ToolGateway):
             self.install_tool_windows(cli_version)
             command_prefix = "./jf.exe"
         self.config_server(command_prefix, token)
+        file_result = self.scan_dependencies(command_prefix)
         return 0
