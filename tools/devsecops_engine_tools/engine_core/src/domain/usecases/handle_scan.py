a368e3b4bed1957691e48f3ea4d546f58af15d01
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-01-31 15:01:59 -0500

    unit test 1

--- tools/devsecops_engine_tools/engine_core/src/domain/usecases/handle_scan.py
+++ tools/devsecops_engine_tools/engine_core/src/domain/usecases/handle_scan.py
@@ -103,10 +103,7 @@ class HandleScan:
         elif "engine_secret" in dict_args["tool"]:
             findings_list, input_core = runner_secret_scan(
                 dict_args,
-                # dict_args["remote_config_repo"],
-                # "SAST/Secret_Scan/configTools.json",
                 config_tool,
-                # "trufflehog"
             )
             return findings_list, input_core
         elif "engine_dependencies" in dict_args["tool"]:
