fe1029c6b588b3c928e40e493831cb9c2ba309cc
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-02-01 10:33:10 -0500

    add unittest to azure folder

---
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/test/infrastructure/driven_adapters/azure_devops/test_azure_devops.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import MagicMock
+from unittest import mock
+from devsecops_engine_tools.engine_sast.engine_secret.src.infrastructure.driven_adapters.azure_devops.azure_devops import (
+    AzureDevops,
+)
+
+
+class TestAzureDevops(unittest.TestCase):
+    @mock.patch(
+        "devsecops_engine_tools.engine_sast.engine_secret.src.infrastructure.driven_adapters.azure_devops.azure_devops.AzureDevopsApi",
+        autospec=True,
+    )
+    @mock.patch(
+        "devsecops_engine_tools.engine_sast.engine_secret.src.infrastructure.driven_adapters.azure_devops.azure_devops.SystemVariables",
+        autospec=True,
+    )
+    def test_get_remote_config(self, mock_system_variables, mock_azure_devops_api):
+        azure_devops = AzureDevops()
+
+        # Set up mock values for SystemVariables
+        mock_system_variables.System_TeamFoundationCollectionUri.value.return_value = (
+            "System_TeamFoundationCollectionUri"
+        )
+
+        # Mock the AzureDevopsApi class
+        mock_azure_devops_api_instance = MagicMock()
+        mock_azure_devops_api_instance.get_azure_connection.return_value = (
+            "MockedConnection"
+        )
+        mock_azure_devops_api_instance.get_remote_json_config.return_value = {
+            "key": "value"
+        }
+        mock_azure_devops_api.return_value = mock_azure_devops_api_instance
+
+        remote_config_repo = "my_repo"
+        remote_config_path = "my_path"
+        result = azure_devops.get_remote_config(remote_config_repo, remote_config_path)
+
+        assert result == {"key": "value"}
+
+    @mock.patch(
+        "devsecops_engine_tools.engine_sast.engine_secret.src.infrastructure.driven_adapters.azure_devops.azure_devops.ReleaseVariables",
+        autospec=True,
+    )
+    def test_get_variable(self, mock_release_variables):
+        azure_devops = AzureDevops()
+
+        # Mock the ReleaseVariables class
+        mock_release_variables.Release_Definitionname.value.return_value = (
+            "Release_Definitionname"
+        )
+
+        result = azure_devops.get_variable("pipeline")
+        assert result == "Release_Definitionname"
