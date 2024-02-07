9fd5fc43769eaf1b35409e704ea916f2a1544277
sarbole <sarbole@bancolombia.com.co>
2024-02-07 15:25:34 -0500

    implement exclusions

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/prisma_cloud/prisma_cloud_manager_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/prisma_cloud/prisma_cloud_manager_scan.py
@@ -16,6 +16,7 @@ from devsecops_engine_utilities import settings
 
 logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
 
+
 class PrismaCloudManagerScan(ToolGateway):
     def download_twistcli(
         self, file_path, prisma_access_key, prisma_secret_key, prisma_console_url
@@ -82,7 +83,9 @@ class PrismaCloudManagerScan(ToolGateway):
                         with open(file_name, "a") as file:
                             file.write(result_file + "\n")
                     except subprocess.CalledProcessError as e:
-                        logger.error(f"Error during image scan of {repository}: {e.stderr}")
+                        logger.error(
+                            f"Error during image scan of {repository}: {e.stderr}"
+                        )
 
         return images_scanned
 
