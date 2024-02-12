eb9f08656bb156314b2ac152eb9e39d7a99cabf5
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-02-12 09:55:19 -0500

    delete version and add exclude_path from configtools

--- tools/devsecops_engine_tools/engine_sast/engine_secret/src/domain/model/DeserializeConfigTool.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/src/domain/model/DeserializeConfigTool.py
@@ -2,8 +2,8 @@ from devsecops_engine_tools.engine_core.src.domain.model.threshold import Thresh
 
 class DeserializeConfigTool:
     def __init__(self, json_data, tool):
-        self.version = json_data[tool]["VERSION"]
         self.ignore_search_pattern = json_data[tool]["IGNORE_SEARCH_PATTERN"]
         self.message_info_sast_build = json_data[tool]["MESSAGE_INFO_SAST_BUILD"]
         self.level_compliance = Threshold(json_data[tool]['THRESHOLD'])
         self.scope_pipeline = ''
+        self.exclude_path = json_data[tool]["EXCLUDE_PATH"]
