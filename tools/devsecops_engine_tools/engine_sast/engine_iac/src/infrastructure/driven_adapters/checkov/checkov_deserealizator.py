d4c79a1c1cb3a4aab9dc0049a41456745307c530
luarredo <luarredo@bancolombia.com>
2024-02-13 09:29:38 -0500

    Refactor CheckovDeserealizator class to improve finding description

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
@@ -18,10 +18,10 @@ class CheckovDeserealizator:
             if "failed_checks" in str(result):
                 for scan in result["results"]["failed_checks"]:
                     finding_open = Finding(
-                        id=rules[scan.get("check_id")].get("customID", scan.get("check_id")),
+                        id=scan.get("check_id"),
                         cvss=None,
                         where=scan.get("repo_file_path"),
-                        description=scan.get("check_name"),
+                        description=rules[scan.get("check_id")].get("checkID", scan.get("check_name")),
                         severity=rules[scan.get("check_id")].get("severity").lower(),
                         identification_date=datetime.now().strftime("%d%m%Y"),
                         module="engine_iac",
