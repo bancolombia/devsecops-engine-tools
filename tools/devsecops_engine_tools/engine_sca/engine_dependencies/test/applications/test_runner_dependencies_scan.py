f2410232662eed770cb339765c34e14e5098e1e7
Santiago Santa <santsant@bancolombia.com.co>
2024-02-16 09:26:08 -0500

    Adding unit test

---
+++ tools/devsecops_engine_tools/engine_sca/engine_dependencies/test/applications/test_runner_dependencies_scan.py
@@ -0,0 +1,28 @@
+from devsecops_engine_tools.engine_sca.engine_dependencies.src.applications.runner_dependencies_scan import (
+    runner_engine_dependencies
+)
+
+import pytest
+from unittest.mock import mock_open, patch
+
+def test_init_engine_dependencies():
+    with patch(
+        "devsecops_engine_tools.engine_sca.engine_dependencies.src.applications.runner_dependencies_scan.XrayScan"
+    ) as mock_tool_run, patch(
+        "devsecops_engine_tools.engine_sca.engine_dependencies.src.applications.runner_dependencies_scan.AzureDevops"
+    ) as mock_tool_remote, patch(
+        "devsecops_engine_tools.engine_sca.engine_dependencies.src.applications.runner_dependencies_scan.XrayDeserializator"
+    ) as mock_tool_deserializator, patch(
+        "devsecops_engine_tools.engine_sca.engine_dependencies.src.applications.runner_dependencies_scan.init_engine_dependencies"
+    ) as mock_init_engine_dependencies:
+        dict_args = {"remote_config_repo": "remote_repo"}
+        token = "token"
+        config_tool = {
+            "ENGINE_DEPENDENCIES": {"ENABLED": "true", "TOOL": "XRAY"},
+        }
+
+        result = runner_engine_dependencies(dict_args, config_tool, token)
+        
+
+        mock_init_engine_dependencies.assert_any_call
+
