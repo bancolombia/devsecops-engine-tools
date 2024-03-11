6f8c8ad157e9f7e45cf71ff0d5017e06de241a4a
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-03-11 10:08:58 -0500

    update version

--- tools/setup.py
+++ tools/setup.py
@@ -13,7 +13,7 @@ def get_requirements():
 
 setup(
     name="devsecops_engine_tools",
-    version="1.6.13",
+    version="1.6.14",
     author="Bancolombia devsecops Team ",
     author_email="devsecops@bancolombia.com.co",
     description="tool for devsecops strategy",
