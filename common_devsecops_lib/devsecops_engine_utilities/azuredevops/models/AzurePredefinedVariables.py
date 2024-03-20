7e55c3192730373fdf377ec9edccb16436d922ca
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-03-20 16:52:42 -0500

    add variable targetbranch

--- common_devsecops_lib/devsecops_engine_utilities/azuredevops/models/AzurePredefinedVariables.py
+++ common_devsecops_lib/devsecops_engine_utilities/azuredevops/models/AzurePredefinedVariables.py
@@ -34,6 +34,7 @@ class SystemVariables(BaseEnum):
     System_TeamProject = "System.TeamProject"
     System_TeamProjectId = "System.TeamProject"
     System_PullRequestId = "System.PullRequest.PullRequestId"
+    System_TargetBranch = "System.PullRequest.targetBranch"
 
 
 class BuildVariables(BaseEnum):
