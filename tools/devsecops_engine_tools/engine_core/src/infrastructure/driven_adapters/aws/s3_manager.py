eb6b416fe11b491d855e21c71accdf9b2d3be390
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2024-04-05 17:55:53 -0500

    refactor s3_manager

--- tools/devsecops_engine_tools/engine_core/src/infrastructure/driven_adapters/aws/s3_manager.py
+++ tools/devsecops_engine_tools/engine_core/src/infrastructure/driven_adapters/aws/s3_manager.py
@@ -12,8 +12,21 @@ boto3.set_stream_logger(name="botocore.credentials", level=logging.WARNING)
 
 
 class S3Manager(MetricsManagerGateway):
+
+    def _get_s3_data(self, client, bucket, path):
+        try:
+            response = client.get_object(
+                Bucket=bucket,
+                Key=path,
+            )
+            return response["Body"].read().decode("utf-8")
+        except client.exceptions.NoSuchKey:
+            return ""
+
     def send_metrics(self, config_tool, tool, file_path):
-        temp_credentials = assume_role(config_tool["METRICS_MANAGER"]["AWS"]["ROLE_ARN"])
+        temp_credentials = assume_role(
+            config_tool["METRICS_MANAGER"]["AWS"]["ROLE_ARN"]
+        )
         session = boto3.session.Session()
         client = session.client(
             service_name="s3",
@@ -22,23 +35,16 @@ class S3Manager(MetricsManagerGateway):
             aws_secret_access_key=temp_credentials["SecretAccessKey"],
             aws_session_token=temp_credentials["SessionToken"],
         )
-        data = ""
         date = datetime.datetime.now()
         path_bucket = f'{tool}/{date.strftime("%Y")}/{date.strftime("%m")}/{date.strftime("%d")}/{file_path.split("/")[-1]}'
-        try:
-            response = client.get_object(
-                Bucket=config_tool["METRICS_MANAGER"]["AWS"]["BUCKET"],
-                Key=path_bucket,
-            )
-            data = response["Body"].read().decode("utf-8")
-        except client.exceptions.NoSuchKey:
-            pass
+
+        data = self._get_s3_data(
+            client, config_tool["METRICS_MANAGER"]["AWS"]["BUCKET"], path_bucket
+        )
 
         with open(file_path, "rb") as new_data:
-            if data != "":
-                data = data + "\n" + new_data.read().decode("utf-8")
-            else:
-                data = new_data.read().decode("utf-8")
+            new_data_content = new_data.read().decode("utf-8")
+            data = data + "\n" + new_data_content if data else new_data_content
             client.put_object(
                 Bucket=config_tool["METRICS_MANAGER"]["AWS"]["BUCKET"],
                 Key=path_bucket,
