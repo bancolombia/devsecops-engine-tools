8b51b46f5a46871c1c62fcc04a4c8307f3e72507
sarbole <sarbole@bancolombia.com.co>
2024-04-15 18:17:57 -0500

    update list image and build id

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/docker/docker_images.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/docker/docker_images.py
@@ -4,24 +4,26 @@ from devsecops_engine_tools.engine_sca.engine_container.src.domain.model.gateway
 )
 import json
 
+import docker
+
+
 
 class DockerImages(ImagesGateway):
     def list_images(self):
-        """
-        Lista las imagenes de Docker en formato json
-        """
-        command = ["docker", "images", "--format", "json"]
-        try:
-            results = subprocess.run(
-                command,
-                check=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True,
-            )
-            results = results.stdout.split("\n")
-            results.pop()  
-            return [json.loads(results[0])] if results else []
+        try:   
+            client = docker.from_env()
 
+            images = client.images.list()
+
+            # Ordenamos las imágenes por fecha de creación en orden descendente
+            images_sorted = sorted(images, key=lambda x: x.attrs['Created'], reverse=True)
+
+            latest_image = images_sorted[0]
+            
+            print("ID de la última imagen:", latest_image.id)
+            print("Tag de la última imagen:", latest_image.tags)
+            print("Fecha de creación de la última imagen:", latest_image.attrs['Created'])
+            
+            return latest_image
         except subprocess.CalledProcessError as e:
-            raise ValueError(f"Error listing images:{e.stderr}")
+                raise ValueError(f"Error listing images:{e.stderr}")
