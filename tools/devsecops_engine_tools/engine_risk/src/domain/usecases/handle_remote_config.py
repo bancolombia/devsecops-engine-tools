be1705ca99c8c6005636ab7f7e9fde900012e833
Santiago Santa <santsant@bancolombia.com.co>
2024-04-17 16:06:32 -0500

    Entry point of engine_risk

---
+++ tools/devsecops_engine_tools/engine_risk/src/domain/usecases/handle_remote_config.py
@@ -0,0 +1,10 @@
+class HandleRemoteConfig:
+    def __init__(
+            self,
+            remote_config,
+            findings_list,
+    ):
+        self.remote_config = remote_config
+        self.findings_list = findings_list
+
+
