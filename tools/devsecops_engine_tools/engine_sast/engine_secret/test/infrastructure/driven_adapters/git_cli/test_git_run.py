64058c4474802fd3228260026c53a21c6c3b8737
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-04-22 10:46:20 -0500

    unit test

--- tools/devsecops_engine_tools/engine_sast/engine_secret/test/infrastructure/driven_adapters/git_cli/test_git_run.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/test/infrastructure/driven_adapters/git_cli/test_git_run.py
@@ -3,74 +3,55 @@ from unittest.mock import patch, MagicMock
 from devsecops_engine_tools.engine_sast.engine_secret.src.infrastructure.driven_adapters.git_cli.git_run import GitRun
 
 class TestGitRun(unittest.TestCase):
-    
+    @patch('os.makedirs')
     @patch('os.chdir')
-    @patch('git.Repo', autospec=True)
-    def test_get_files_pull_request(self, mock_repo, mock_chdir):
-        # Arrange
+    @patch('git.Repo')
+    @patch('subprocess.run')
+    def test_get_files_pull_request_Exception(self, mock_subprocess_run, mock_git_repo, mock_os_chdir, mock_os_makedirs):
         sys_working_dir = "/azp/_work/1/s"
         target_branch = "trunk"
         config_target_branch = ["trunk", "develop"]
         source_branch = "refs/heads/feature/branch"
-        mock_repo_instance = MagicMock()
-        mock_repo.return_value = mock_repo_instance
-        mock_repo_instance.git.diff.return_value = "file1.py\nfile2.py\n"
+        access_token = "ABCDEFG123456"
+        collection_uri = "https://dev.azure.com/orgName"
+        team_project = "team_project"
+        repository_name = "NU00001_Repo_test"
+
+        mock_git_repo.side_effect = Exception("Simulated exception")
         
         git_run = GitRun()
-        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch)
+        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch,
+                                               access_token, collection_uri, team_project, repository_name)
         
-        self.assertEqual(files, ["file1.py", "file2.py"])   
-        
-    @patch('git.Repo', autospec=True)
-    def test_get_git_source_branch(self, mock_repo):
-        repository = MagicMock()
-        mock_repo.return_value = repository
-        repository.remotes.origin.fetch.return_value = ["branch1", "branch2"]
-        
-        git_run = GitRun()
-        source_branch = git_run.get_git_source_branch(repository, "refs/heads/source_branch")
-        
-        repository.remotes.origin.fetch.assert_called_once()
-        self.assertEqual(source_branch, "source_branch")
-
-    def test_get_files_pull_request_no_target_branch(self):
-        git_run = GitRun()
-        sys_working_dir = '/azp/_work/1/s'
-        target_branch = 'trunk'
-        config_target_branch = ['main', 'develop']
-        source_branch = 'refs/heads/feature/branch'
-
-        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch)
-
-        self.assertEqual(files, [])
-
-    @patch('os.chdir')
-    @patch('git.Repo')
-    def test_get_files_pull_request_fail_diff(self, mock_repo, mock_chdir):
-        git_run = GitRun()
-        sys_working_dir = '/azp/_work/1/s'
-        target_branch = 'main'
-        config_target_branch = ['main', 'develop']
-        source_branch = 'refs/heads/feature/branch'
- 
-        mock_repo.side_effect = Exception("Simulated exception")
-
-        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch)
-
         self.assertEqual(files, [])
         
+    @patch('os.makedirs')
     @patch('os.chdir')
     @patch('git.Repo')
-    def test_get_files_pull_request_no_target_branch(self, mock_repo, mock_chdir):
-        repository = MagicMock()
-        mock_repo.return_value = repository
-        repository.remotes.origin.fetch.side_effect = Exception("Simulated exception")
-        
+    @patch('subprocess.run')
+    def test_get_files_pull_request(self, mock_subprocess_run, mock_git_repo, mock_os_chdir, mock_os_makedirs):
+        sys_working_dir = "/azp/_work/1/s"
+        target_branch = "trunk"
+        config_target_branch = ["trunk", "develop"]
+        source_branch = "refs/heads/feature/branch"
+        access_token = "ABCDEFG123456"
+        collection_uri = "https://dev.azure.com/orgName"
+        team_project = "team_project"
+        repository_name = "NU00001_Repo_test"
+
+        mock_repo = MagicMock()
+        mock_repo.git.diff.return_value = "file1.py\nfile2.py"
+        mock_git_repo.return_value = mock_repo
+        mock_subprocess_run.return_value = MagicMock()
+
         git_run = GitRun()
-        source_branch = git_run.get_git_source_branch(repository, "refs/heads/source_branch")
-        
-        repository.remotes.origin.fetch.assert_called_once()
-        self.assertEqual(source_branch, None)
+        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch,
+                                               access_token, collection_uri, team_project, repository_name)
+
+        self.assertEqual(files, ["file1.py", "file2.py"])
+        mock_os_makedirs.assert_called_once_with("/azp/_work/1/s/NU00001_Repo_test")
+        mock_os_chdir.assert_called_with("/azp/_work/1/s/NU00001_Repo_test")
+        mock_git_repo.assert_called_once_with("/azp/_work/1/s/NU00001_Repo_test")
         
 if __name__ == '__main__':
     unittest.main()
