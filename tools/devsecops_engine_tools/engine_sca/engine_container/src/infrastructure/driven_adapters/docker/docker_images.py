097602b0cc35a2423f7d841adc88908482bd566d
sarbole <sarbole@bancolombia.com.co>
2024-04-22 10:51:46 -0500

    unit test and prints

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/docker/docker_images.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/docker/docker_images.py
@@ -12,18 +12,12 @@ class DockerImages(ImagesGateway):
     def list_images(self):
         try:   
             client = docker.from_env()
-
             images = client.images.list()
-
-            # Ordenamos las imágenes por fecha de creación en orden descendente
             images_sorted = sorted(images, key=lambda x: x.attrs['Created'], reverse=True)
-
-            latest_image = images_sorted[0]
-            
+            latest_image = images_sorted[0]         
             print("ID last image:", latest_image.id)
             print("Tag last image:", latest_image.tags)
             print("Created date last image:", latest_image.attrs['Created'])
-            
             return latest_image
         except subprocess.CalledProcessError as e:
                 raise ValueError(f"Error listing images:{e.stderr}")
