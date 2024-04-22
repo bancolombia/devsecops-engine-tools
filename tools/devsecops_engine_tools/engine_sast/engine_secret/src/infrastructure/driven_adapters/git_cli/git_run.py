9bc44f1ac612563f2a3f02efb9db1f90b79ada7a
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-04-22 10:46:20 -0500

    unit test

--- tools/devsecops_engine_tools/engine_sast/engine_secret/src/infrastructure/driven_adapters/git_cli/git_run.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/src/infrastructure/driven_adapters/git_cli/git_run.py
@@ -30,25 +30,20 @@ class GitRun(GitGateway):
             f"{repository_name}"
             )
             
-            base_compact_url = 'https://grupobancolombia.visualstudio.com/Vicepresidencia%20Servicios%20de%20Tecnolog%C3%ADa/_git/AW1122003_Cliente_Fork'
             url_sin_https = base_compact_url.replace("https://", "")
             url_with_token = f"https://x-access-token:{access_token}@{url_sin_https}"
 
             ruta_nueva_carpeta = sys_working_dir + '/' + repository_name
             os.makedirs(ruta_nueva_carpeta)
             os.chdir(sys_working_dir)
-            subprocess.run(["git", "clone", url_with_token, ruta_nueva_carpeta])
-            # subprocess.run(["git", "clone", url_with_token, ruta_nueva_carpeta], capture_output=True, text=True)
+            subprocess.run(["git", "clone", url_with_token, ruta_nueva_carpeta], capture_output=True, text=True)
             os.chdir(ruta_nueva_carpeta)
-            subprocess.run(["git", "branch", "-a"])
+            
             repository = git.Repo(ruta_nueva_carpeta)
 
             source_branch = source_branch.replace("refs/heads/", "")
-            source_branch = 'develop'
             subprocess.run(["git", "checkout", f"origin/{source_branch}"], capture_output=True, text=True)
             if source_branch != None:
-                source_branch = 'develop'
-                target_branch = 'master'
                 diff = repository.git.diff(f"origin/{source_branch}..origin/{target_branch}", name_only=True)
                 if diff:
                     diff_files = diff.strip().split("\n")
