61c8d608f751ca46b83a6b1f4d10ccaa6fa1951d
sarbole <sarbole@bancolombia.com.co>
2024-04-22 10:51:46 -0500

    unit test and prints

--- tools/devsecops_engine_tools/engine_sca/engine_container/test/infrastructure/driven_adapters/docker/test_docker_images.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/test/infrastructure/driven_adapters/docker/test_docker_images.py
@@ -1,25 +1,35 @@
-import subprocess
 from unittest.mock import patch
-
 import pytest
+from unittest.mock import MagicMock
 from devsecops_engine_tools.engine_sca.engine_container.src.infrastructure.driven_adapters.docker.docker_images import DockerImages
-@pytest.fixture
-def mock_subprocess_run_success(monkeypatch):
-    def mock_run(*args, **kwargs):
-        class CompletedProcess:
-            def __init__(self, stdout, stderr, returncode):
-                self.stdout = stdout
-                self.stderr = stderr
-                self.returncode = returncode
-
-        stdout = '{"Containers":"N/A","CreatedAt":"2024-01-17 16:33:26 -0500 -05","CreatedSince":"6 weeks ago","Digest":"\\u003cnone\\u003e","ID":"3e594e5ad3ab","Repository":"engine-dev-ecr","SharedSize":"N/A","Size":"7.34MB","Tag":"db-trunk-trunk.20240126.1","UniqueSize":"N/A","VirtualSize":"7.335MB"}\n{"Containers":"N/A","CreatedAt":"2024-01-17 16:33:26 -0500 -05","CreatedSince":"6 weeks ago","Digest":"\\u003cnone\\u003e","ID":"3e594e5ad3ab","Repository":"cops-engine-dev-ecr","SharedSize":"N/A","Size":"7.34MB","Tag":"20240126.2","UniqueSize":"N/A","VirtualSize":"7.335MB"}\n'
-        return CompletedProcess(stdout, b'', 0)
-
-    monkeypatch.setattr(subprocess, 'run', mock_run)
 
 
+        
 class TestDockerImages:
-    def test_list_images_success(self, mock_subprocess_run_success):
-        docker_images = DockerImages()
-        images = docker_images.list_images()
-        assert images == [{'Containers': 'N/A', 'CreatedAt': '2024-01-17 16:33:26 -0500 -05', 'CreatedSince': '6 weeks ago', 'Digest': '<none>', 'ID': '3e594e5ad3ab', 'Repository': 'engine-dev-ecr', 'SharedSize': 'N/A', 'Size': '7.34MB', 'Tag': 'db-trunk-trunk.20240126.1', 'UniqueSize': 'N/A', 'VirtualSize': '7.335MB'}]
+    @pytest.fixture
+    def docker_images(self):
+        with patch('docker.from_env') as mock_from_env:
+            mock_client = MagicMock()
+            mock_from_env.return_value = mock_client
+            yield DockerImages()
+
+    def test_list_images(self, docker_images):
+        mock_images_list = [
+            MagicMock(id='image_id_1', tags=['tag1'], attrs={'Created': '2024-01-01T00:00:00Z'}),
+            MagicMock(id='image_id_2', tags=['tag2'], attrs={'Created': '2024-02-02T00:00:00Z'}),
+            MagicMock(id='image_id_3', tags=['tag3'], attrs={'Created': '2024-03-03T00:00:00Z'}),
+        ]
+
+        with patch('docker.from_env') as mock_from_env:
+            mock_client = MagicMock()
+            mock_from_env.return_value = mock_client
+            docker_images.client = mock_client
+
+            mock_client.images.list.return_value = mock_images_list
+
+            latest_image = docker_images.list_images()
+
+            mock_client.images.list.assert_called_once()
+            assert latest_image.id == 'image_id_3'
+            assert latest_image.tags == ['tag3']
+            assert latest_image.attrs['Created'] == '2024-03-03T00:00:00Z'
