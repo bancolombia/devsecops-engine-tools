d681f44acc9acfbb624415ba49681d2f15a2fc98
carlos javier lopez ortega <carloslo@bancolombia.com.co>
2024-04-23 09:51:54 -0500

    control if folder exist

--- tools/devsecops_engine_tools/engine_sast/engine_secret/test/infrastructure/driven_adapters/git_cli/test_git_run.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/test/infrastructure/driven_adapters/git_cli/test_git_run.py
@@ -25,11 +25,12 @@ class TestGitRun(unittest.TestCase):
         
         self.assertEqual(files, [])
         
+    @patch('os.path.exists')
     @patch('os.makedirs')
     @patch('os.chdir')
     @patch('git.Repo')
     @patch('subprocess.run')
-    def test_get_files_pull_request(self, mock_subprocess_run, mock_git_repo, mock_os_chdir, mock_os_makedirs):
+    def test_get_files_pull_request_path_no_exist(self, mock_subprocess_run, mock_git_repo, mock_os_chdir, mock_os_makedirs, mock_path_exists):
         sys_working_dir = "/azp/_work/1/s"
         target_branch = "trunk"
         config_target_branch = ["trunk", "develop"]
@@ -43,7 +44,8 @@ class TestGitRun(unittest.TestCase):
         mock_repo.git.diff.return_value = "file1.py\nfile2.py"
         mock_git_repo.return_value = mock_repo
         mock_subprocess_run.return_value = MagicMock()
-
+        mock_path_exists.return_value = False
+        
         git_run = GitRun()
         files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch,
                                                access_token, collection_uri, team_project, repository_name)
@@ -53,5 +55,39 @@ class TestGitRun(unittest.TestCase):
         mock_os_chdir.assert_called_with("/azp/_work/1/s/NU00001_Repo_test")
         mock_git_repo.assert_called_once_with("/azp/_work/1/s/NU00001_Repo_test")
         
+    @patch('os.path.exists')
+    def test_get_files_pull_request_path_exist(self, mock_path_exists):
+        sys_working_dir = "/azp/_work/1/s"
+        target_branch = "trunk"
+        config_target_branch = ["trunk", "develop"]
+        source_branch = "refs/heads/feature/branch"
+        access_token = "ABCDEFG123456"
+        collection_uri = "https://dev.azure.com/orgName"
+        team_project = "team_project"
+        repository_name = "NU00001_Repo_test"
+   
+        mock_path_exists.return_value = True
+        
+        git_run = GitRun()
+        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch,
+                                               access_token, collection_uri, team_project, repository_name)
+
+        self.assertEqual(files, [])
+        
+    def test_get_files_pull_request_no_target_branch(self):
+        sys_working_dir = "/azp/_work/1/s"
+        target_branch = "release"
+        config_target_branch = ["trunk", "develop"]
+        source_branch = "refs/heads/feature/branch"
+        access_token = "ABCDEFG123456"
+        collection_uri = "https://dev.azure.com/orgName"
+        team_project = "team_project"
+        repository_name = "NU00001_Repo_test"
+        
+        git_run = GitRun()
+        files = git_run.get_files_pull_request(sys_working_dir, target_branch, config_target_branch, source_branch,
+                                               access_token, collection_uri, team_project, repository_name)
+
+        self.assertEqual(files, [])
 if __name__ == '__main__':
     unittest.main()
