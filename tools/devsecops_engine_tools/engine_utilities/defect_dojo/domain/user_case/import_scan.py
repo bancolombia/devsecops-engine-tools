32e37c438359d207d221332e61ba680d8e879aee
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2024-04-26 17:38:41 -0500

    fix integraion tests

--- tools/devsecops_engine_tools/engine_utilities/defect_dojo/domain/user_case/import_scan.py
+++ tools/devsecops_engine_tools/engine_utilities/defect_dojo/domain/user_case/import_scan.py
@@ -1,6 +1,5 @@
 import re
 import os
-import csv
 from devsecops_engine_tools.engine_utilities.utils.api_error import ApiError
 from devsecops_engine_tools.engine_utilities.settings import SETTING_LOGGER
 from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
@@ -9,10 +8,6 @@ from devsecops_engine_tools.engine_utilities.defect_dojo.infraestructure.driver_
 from devsecops_engine_tools.engine_utilities.defect_dojo.infraestructure.driver_adapters.product import ProductRestConsumer
 from devsecops_engine_tools.engine_utilities.defect_dojo.infraestructure.driver_adapters.scan_configurations import (
     ScanConfigrationRestConsumer,
-)
-from devsecops_engine_tools.engine_utilities.defect_dojo.domain.models.scan_configuration import (
-    ScanConfiguration,
-    ScanConfigurationList,
 )
 from devsecops_engine_tools.engine_utilities.defect_dojo.infraestructure.driver_adapters.engagement import EngagementRestConsumer
 from devsecops_engine_tools.engine_utilities.defect_dojo.domain.request_objects.import_scan import ImportScanRequest
@@ -40,48 +35,43 @@ class ImportScanUserCase:
 
     def execute(self, request: ImportScanRequest) -> ImportScanRequest:
         response = None
-        product_type_id = None
         product_id = None
+
         if (request.product_name or request.product_type_name) == "":
-            log = f"Name product {request.product_name} not found"
+            log = f"Name product {request.product_name} or product type {request.product_type_name} is empty"
             logger.error(log)
             raise ApiError(log)
-
+        
         logger.info(f"Match {request.scan_type}")
-        product_types = self.__rest_product_type.get_product_types(request.product_type_name)
-        if product_types.results == []:
-            product_type = self.__rest_product_type.post_product_type(request.product_type_name)
-            product_type_id = product_type.id
-            logger.info(f"product_type created: {product_type.name} with id {product_type.id}")
-        else:
-            if len(product_types.results) != 1:
-                logger.warning(f"there is more than one product type with the name: {product_types.results}")
-
-            product_type_id = product_types.results[0].id
-            logger.info(
-                f"product_type found: {product_types.results[0].name}\
-                    with id {product_type_id}"
-            )
-
         products = self.__rest_product.get_products(request)
-        if products.results == []:
-            product = self.__rest_product.post_product(request, product_type_id)
-            product_id = product.id
-            logger.info(
-                f"product created: {product.name}\
-                    found with id: {product.id}"
-            )
-        else:
-            # Product_type name is unique and product_name is unique
+        if len(products.results) > 0:
             product_id = products.results[0].id
-            if products.results[0].prod_type != product_type_id:
-                logger.warning(
-                    f"The product: {product_id} does not belong to the product_type con id: {product_type_id}"
+            request.product_name = products.results[0].name
+            request.product_type_name = self.__rest_product_type.get_product_type_id(products.results[0].prod_type).name
+            logger.info(f"product found: {request.product_name} with id: {product_id}")
+        else:
+            product_type_id = None
+            product_types = self.__rest_product_type.get_product_types(request.product_type_name)
+            if product_types.results == []:
+                product_type = self.__rest_product_type.post_product_type(request.product_type_name)
+                product_type_id = product_type.id
+                logger.info(f"product_type created: {product_type.name} with id {product_type.id}")
+            else:
+                if len(product_types.results) != 1:
+                    logger.warning(f"there is more than one product type with the name: {product_types.results}")
+
+                product_type_id = product_types.results[0].id
+                logger.info(
+                    f"product_type found: {product_types.results[0].name}\
+                        with id {product_type_id}"
                 )
-            logger.info(
-                f"product found: {request.product_name}\
-                with id: {product_id}"
-            )
+
+                product = self.__rest_product.post_product(request, product_type_id)
+                product_id = product.id
+                logger.info(
+                    f"product created: {product.name}\
+                        found with id: {product.id}"
+                )
 
         api_scan_bool = re.search(" API ", request.scan_type)
         if api_scan_bool:
