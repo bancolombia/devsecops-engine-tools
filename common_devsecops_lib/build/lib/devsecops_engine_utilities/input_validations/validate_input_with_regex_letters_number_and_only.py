7bc5257a67d075f1c164e2bebb49ce05fdd91177
Santiago Santa <santsant@bancolombia.com.co>
2024-05-06 16:26:21 -0500

    Delete common_devsecops_lib folder

--- common_devsecops_lib/build/lib/devsecops_engine_utilities/input_validations/validate_input_with_regex_letters_number_and_only.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def validate_input_with_regex(value, input_name):
-    pattern = r"^[a-zA-Z0-9_]+$"
-    if not re.match(pattern, value):
-        raise ValueError(f"Error: Invalid input for {input_name}. Only letters and numbers are allowed.")
-    return value
