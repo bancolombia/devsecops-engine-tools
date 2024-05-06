7de53eabcef27016d5bb0187de02b8fb77299e78
Santiago Santa <santsant@bancolombia.com.co>
2024-05-06 16:52:10 -0500

    Changes to logger in engine risk

--- tools/devsecops_engine_tools/engine_risk/src/applications/runner_engine_risk.py
+++ tools/devsecops_engine_tools/engine_risk/src/applications/runner_engine_risk.py
@@ -2,8 +2,8 @@ from devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_po
     init_engine_risk,
 )
 
-from devsecops_engine_utilities.utils.logger_info import MyLogger
-from devsecops_engine_utilities import settings
+from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
+from devsecops_engine_tools.engine_utilities import settings
 
 
 logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
