b66a7d649b5490088825f3d707d4b6dd1a1074a3
Santiago Santa <santsant@bancolombia.com.co>
2024-05-10 16:16:05 -0500

    Change log for vulnerability list

--- tools/devsecops_engine_tools/engine_risk/src/domain/usecases/break_build.py
+++ tools/devsecops_engine_tools/engine_risk/src/domain/usecases/break_build.py
@@ -22,7 +22,7 @@ class BreakBuild:
         devops_platform_gateway = self.devops_platform_gateway
         printer_table_gateway = self.printer_table_gateway
         if len(report_list):
-            print("Below are all vulnerabilities.")
+            print("Below are all vulnerabilities from Vulnerability Management Platform")
             printer_table_gateway.print_table_report(
                 report_list,
             )
