e94d3dbda189b385dd6c2342934c1020d9691b6c
Santiago Santa <santsant@bancolombia.com.co>
2024-05-15 11:43:22 -0500

    Solving comments 1

--- tools/devsecops_engine_tools/engine_risk/test/applications/test_runner_engine_risk.py
+++ tools/devsecops_engine_tools/engine_risk/test/applications/test_runner_engine_risk.py
@@ -35,18 +35,15 @@ def test_runner_engine_risk(mock_init_engine_risk):
 @patch(
     "devsecops_engine_tools.engine_risk.src.applications.runner_engine_risk.logger.error"
 )
-@patch("subprocess.run")
 def test_runner_engine_risk_exception(
-    mock_subprocess, mock_logger_error, mock_init_engine_risk
+    mock_logger_error, mock_init_engine_risk
 ):
     devops_platform_gateway = "devops_platform_gateway"
     print_table_gateway = "print_table_gateway"
     dict_args = {"key": "value"}
     findings = []
 
-    mock_subprocess.side_effect = [
-        subprocess.CalledProcessError(returncode=1, cmd=""),
-    ]
+    mock_init_engine_risk.side_effect = Exception("error")
 
     runner_engine_risk(
         dict_args,
@@ -55,4 +52,4 @@ def test_runner_engine_risk_exception(
         print_table_gateway,
     )
 
-    mock_logger_error.assert_any_call
+    mock_logger_error.assert_called_with("Error in engine risk: error")
