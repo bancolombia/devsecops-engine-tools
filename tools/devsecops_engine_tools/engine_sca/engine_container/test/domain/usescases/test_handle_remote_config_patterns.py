1df3955cb79fc91f6c088368ef15508ceec6e435
sarbole <sarbole@bancolombia.com.co>
2024-05-20 23:25:50 -0500

    refactor unit test

--- tools/devsecops_engine_tools/engine_sca/engine_container/test/domain/usescases/test_handle_remote_config_patterns.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/test/domain/usescases/test_handle_remote_config_patterns.py
@@ -1,51 +1,39 @@
 import pytest
-from unittest.mock import MagicMock
-from devsecops_engine_tools.engine_core.src.domain.model.gateway.devops_platform_gateway import DevopsPlatformGateway
 from devsecops_engine_tools.engine_sca.engine_container.src.domain.usecases.handle_remote_config_patterns import HandleRemoteConfigPatterns
 
+@pytest.fixture
+def remote_config():
+    return {"IGNORE_SEARCH_PATTERN": "ignore_this_pipeline"}
+
 
 @pytest.fixture
-def mock_devops_gateway():
-    return MagicMock(spec=DevopsPlatformGateway)
-
-@pytest.fixture
-def handle_remote(mock_devops_gateway):
-    return HandleRemoteConfigPatterns(mock_devops_gateway, {"remote_config_repo": "dummy_repo"})
-
-def test_get_remote_config(mock_devops_gateway, handle_remote):
-    mock_devops_gateway.get_remote_config.return_value = {"dummy_key": "dummy_value"}
-
-    remote_config = handle_remote.get_remote_config("dummy_file_path")
-
-    mock_devops_gateway.get_remote_config.assert_called_once_with("dummy_repo", "dummy_file_path")
-    assert remote_config == {"dummy_key": "dummy_value"}
-
-def test_get_variable(mock_devops_gateway, handle_remote):
-    mock_devops_gateway.get_variable.return_value = "dummy_variable_value"
-
-    variable = handle_remote.get_variable("dummy_variable")
-
-    mock_devops_gateway.get_variable.assert_called_once_with("dummy_variable")
-    assert variable == "dummy_variable_value"
-
-def test_handle_skip_tool(handle_remote):
-    exclusions = {
+def exclusions():
+    return {
         "dummy_pipeline": {
             "SKIP_TOOL": 1
         }
     }
 
-    result = handle_remote.handle_skip_tool(exclusions, "dummy_pipeline")
 
-    assert result is True
+@pytest.fixture
+def handle_remote(remote_config, exclusions):
+    return HandleRemoteConfigPatterns(remote_config, exclusions, "dummy_pipeline")
 
-def test_ignore_analysis_pattern_false(mock_devops_gateway, handle_remote):
-    mock_devops_gateway.get_remote_config.return_value = {"IGNORE_SEARCH_PATTERN": "dummy_pattern"}
-    mock_devops_gateway.get_variable.return_value = "dummy_pattern"
 
-    result = handle_remote.ignore_analysis_pattern()
+def test_ignore_analysis_pattern_false(handle_remote):
+    handle_remote.pipeline_name = "ignore_this_pipeline"
+    assert not handle_remote.ignore_analysis_pattern()
 
-    mock_devops_gateway.get_remote_config.assert_called_once_with('dummy_repo', 'engine_sca/engine_container/ConfigTool.json')
-    mock_devops_gateway.get_variable.assert_called_once_with("pipeline_name")
 
-    assert result is False
+def test_ignore_analysis_pattern_true(handle_remote):
+    handle_remote.pipeline_name = "do_not_ignore_this_pipeline"
+    assert handle_remote.ignore_analysis_pattern()
+
+
+def test_skip_from_exclusion(handle_remote):
+    assert handle_remote.skip_from_exclusion()
+
+
+def test_not_skip_from_exclusion(handle_remote):
+    handle_remote.pipeline_name = "another_pipeline"
+    assert not handle_remote.skip_from_exclusion()
