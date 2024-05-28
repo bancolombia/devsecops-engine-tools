376dd2c318212cf80c549b91f67ce86761a9652a
ssantaa9 <santsant@bancolombia.com.co>
2024-05-28 17:27:46 -0500

    Fixes to container architecture

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/usecases/container_sca_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/usecases/container_sca_scan.py
@@ -8,6 +8,7 @@ from devsecops_engine_tools.engine_sca.engine_container.src.domain.model.gateway
     DeseralizatorGateway,
 )
 
+import os
 
 class ContainerScaScan:
     def __init__(
@@ -34,23 +35,49 @@ class ContainerScaScan:
             list: List of processed images.
         """
         return self.tool_images.list_images()
+    
+    def get_images_already_scanned(self):
+        """
+        Create images scanned file if it does not exist and get the images that have already been scanned.
+        """
+        scanned_images_file = os.path.join(os.getcwd(), "scanned_images.txt")
+        if not os.path.exists(scanned_images_file):
+            open(scanned_images_file, "w").close()
+        with open(scanned_images_file, "r") as file:
+            images_scanned = file.read().splitlines()
+        return images_scanned
+    
+    def set_image_scanned(self, result_file):
+        """
+        Write in scanned_images.txt the result file
+        """
+        with open("scanned_images.txt", "a") as file:
+            file.write(result_file + "\n")
 
     def process(self):
         """
         Process SCA scanning.
 
         Returns:
-            dict: SCA scanning results.
+            string: file scanning results name.
         """
         latest_image = self.get_latest_image()
-        if self.build_id in latest_image.tags[0]:
-            return self.tool_run.run_tool_container_sca(
-                self.remote_config, self.token, latest_image
+        image_name = latest_image.tags[0]
+        image_scanned = None
+        if self.build_id in image_name:
+            result_file = image_name + "_scan_result.json"
+            if result_file in self.get_images_already_scanned():
+                print(f"The image {image_name} has already been scanned previously.")
+                return image_scanned
+            image_scanned = self.tool_run.run_tool_container_sca(
+                self.remote_config, self.token, image_name, result_file
             )
-        print(
-            f"{latest_image.tags[0]} name does not contain build number {self.build_id}. Tool skipped."
-        )
-        return []
+            self.set_image_scanned(result_file)
+        else:
+            print(
+                f"{image_name} name does not contain build number {self.build_id}. Tool skipped."
+            )
+        return image_scanned
 
     def deseralizator(self, image_scanned):
         """
--- tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_deserialize_output.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/infrastructure/driven_adapters/trivy_tool/trivy_deserialize_output.py
@@ -12,43 +12,42 @@ from datetime import datetime, timezone
 
 @dataclass
 class TrivyDeserializator(DeseralizatorGateway):
-    def get_list_findings(self, images_scanned: list) -> "list[Finding]":
+    def get_list_findings(self, image_scanned) -> "list[Finding]":
         list_open_vulnerabilities = []
-        for image in images_scanned:
-            with open(image, "rb") as file:
-                image_object = file.read()
-                json_data = json.loads(image_object)
-                vulnerabilities_data = json_data["Results"][0]["Vulnerabilities"]
-                vulnerabilities = [
-                    Finding(
-                        id=vul.get("VulnerabilityID", ""),
-                        cvss=str(next(
-                            (
-                                v["V3Score"]
-                                for v in vul["CVSS"].values()
-                                if "V3Score" in v
-                            ),
-                            None,
-                        )),
-                        where=vul.get("PkgName", "")
-                        + " "
-                        + vul.get("InstalledVersion", ""),
-                        description=vul.get("Description", "").replace("\n", "")[:150],
-                        severity=vul.get("Severity", "").lower(),
-                        identification_date=datetime.now().strftime(
-                            "%Y-%m-%dT%H:%M:%S%z"
+        with open(image_scanned, "rb") as file:
+            image_object = file.read()
+            json_data = json.loads(image_object)
+            vulnerabilities_data = json_data["Results"][0]["Vulnerabilities"]
+            vulnerabilities = [
+                Finding(
+                    id=vul.get("VulnerabilityID", ""),
+                    cvss=str(next(
+                        (
+                            v["V3Score"]
+                            for v in vul["CVSS"].values()
+                            if "V3Score" in v
                         ),
-                        published_date_cve=datetime.strptime(
-                            vul.get("PublishedDate"),
-                            "%Y-%m-%dT%H:%M:%S.%fZ"
-                            ).replace(tzinfo=timezone.utc).isoformat(),
-                        module="engine_container",
-                        category=Category.VULNERABILITY,
-                        requirements=vul.get("FixedVersion") or vul.get("Status", ""),
-                        tool="Trivy",
-                    )
-                    for vul in vulnerabilities_data
-                    if "CVSS" in vul
-                ]
-                list_open_vulnerabilities.extend(vulnerabilities)
+                        None,
+                    )),
+                    where=vul.get("PkgName", "")
+                    + " "
+                    + vul.get("InstalledVersion", ""),
+                    description=vul.get("Description", "").replace("\n", "")[:150],
+                    severity=vul.get("Severity", "").lower(),
+                    identification_date=datetime.now().strftime(
+                        "%Y-%m-%dT%H:%M:%S%z"
+                    ),
+                    published_date_cve=datetime.strptime(
+                        vul.get("PublishedDate"),
+                        "%Y-%m-%dT%H:%M:%S.%fZ"
+                        ).replace(tzinfo=timezone.utc).isoformat(),
+                    module="engine_container",
+                    category=Category.VULNERABILITY,
+                    requirements=vul.get("FixedVersion") or vul.get("Status", ""),
+                    tool="Trivy",
+                )
+                for vul in vulnerabilities_data
+                if "CVSS" in vul
+            ]
+            list_open_vulnerabilities.extend(vulnerabilities)
         return list_open_vulnerabilities
