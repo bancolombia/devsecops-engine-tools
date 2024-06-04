0b7db28ed83b95a9bd4b92ed8f8b682b358ae992
Michael David Herrera Vargas <micherre@bancolombia.com.co>
2024-06-03 19:35:43 -0500

    KICS Driven Adapter

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/applications/runner_iac_scan.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/applications/runner_iac_scan.py
@@ -4,15 +4,19 @@ from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.entry_poin
 from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.driven_adapters.checkov.checkov_tool import (
     CheckovTool
 )
+from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.driven_adapters.kics.kics_tool import (
+    KicsTool
+)
 
 
 def runner_engine_iac(dict_args, tool, secret_tool, devops_platform_gateway, env):
     try:
         # Define driven adapters for gateways
         tool_gateway = None
-        if (tool == "CHECKOV"):
+        if tool == "CHECKOV":
             tool_gateway = CheckovTool()
-
+        elif tool == "KICS":
+            tool_gateway = KicsTool()
         return init_engine_sast_rm(
             devops_platform_gateway=devops_platform_gateway,
             tool_gateway=tool_gateway,
