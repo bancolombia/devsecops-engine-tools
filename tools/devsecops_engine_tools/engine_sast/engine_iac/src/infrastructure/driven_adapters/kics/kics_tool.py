4ae4d27b08f621fb706f4c2e11e119747a7f61f2
Michael David Herrera Vargas <micherre@bancolombia.com.co>
2024-06-07 15:41:04 -0500

    derealizer adjustment and code reformatting

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/kics/kics_tool.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/kics/kics_tool.py
@@ -3,6 +3,7 @@ import requests
 import zipfile
 import os
 import json
+import glob
 from devsecops_engine_tools.engine_sast.engine_iac.src.domain.model.gateways.tool_gateway import (
     ToolGateway,
 )
@@ -19,10 +20,17 @@ logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
 
 
 class KicsTool(ToolGateway):
+
     def run_tool(
             self, config_tool: ConfigTool, folders_to_scan, environment, container_platform, secret_tool
     ):
-        self.download_repo(folders_to_scan)
+
+        zip_file_name = self.download_latest_zip()
+        self.unzip_repo(zip_file_name)
+        directory = self.generate_binary()
+
+        self.execute_kics(folders_to_scan, directory)
+
         filtered_results = self.get_findings()
 
         kics_deserealizator = KicsDeserealizator()
@@ -32,46 +40,51 @@ class KicsTool(ToolGateway):
 
         return finding_list, generate_file
 
-    def download_repo(self, folders_to_scan):
+    def download_latest_zip(self):
 
         owner = "Checkmarx"
         repository_name = "kics"
-
-        url = f"https://github.com/{owner}/{repository_name}/archive/refs/heads/master.zip"
+        url = f"https://api.github.com/repos/{owner}/{repository_name}/releases/latest"
 
         response = requests.get(url)
 
-        if response.status_code == 200:
-            zip_filnename = f"{repository_name}.zip"
+        data = json.loads(response.text)
 
-            with open(zip_filnename, "wb") as file:
-                file.write(response.content)
+        zip_url = data["zipball_url"]
 
-            self.unzip_repo(zip_filnename, folders_to_scan)
+        zip_response = requests.get(zip_url)
 
-    def unzip_repo(self, zip_filename, folders_to_scan):
+        zip_file_name = f"{repository_name}_latest.zip"
+        with open(zip_file_name, "wb") as file:
+            file.write(zip_response.content)
+
+        return zip_file_name
+
+    def unzip_repo(self, zip_filename):
 
         with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
             zip_ref.extractall()
 
         os.remove(zip_filename)
 
-        self.generate_binary(folders_to_scan)
+    def generate_binary(self):
 
-    def generate_binary(self, folders_to_scan):
+        directories = glob.glob('Checkmarx-kics-*')
+        directory = directories[0]
 
-        os.chdir('./kics-master')
+        os.chdir(f"./{directory}")
 
         subprocess.run(['go', 'mod', 'vendor'], capture_output=True, text=True)
         subprocess.run("go build -o ./bin/kics cmd/console/main.go", capture_output=True, text=True)
 
         os.chdir('..')
-        self.execute_kics(folders_to_scan)
 
-    def execute_kics(self, folders_to_scan):
+        return directory
+
+    def execute_kics(self, folders_to_scan, directory):
         try:
             for folder in folders_to_scan:
-                command = f"./kics-master/bin/kics scan -p {folder} -q ./kics-master/assets --report-formats json -o ./results"
+                command = f"./{directory}/bin/kics scan -p {folder} -q ./{directory}/assets --report-formats json -o ./results"
                 subprocess.run(command, capture_output=True)
 
         except Exception as ex:
@@ -86,12 +99,13 @@ class KicsTool(ToolGateway):
             severity = query.get("severity", "").upper()
             if severity in {"LOW", "MEDIUM", "HIGH", "CRITICAL"}:
                 description = query.get("description", "")
+                query_id = query.get("query_id", "")
                 for file in query.get("files", []):
                     file_name = file.get("file_name", "")
                     filtered_results.append({
                         "severity": severity,
                         "description": description,
-                        "file_name": file_name
+                        "file_name": file_name,
+                        "id": query_id
                     })
         return filtered_results
-
