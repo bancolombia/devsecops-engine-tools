b37b678da104d42573ca272a44c7c7a8b9ec654b
sarbole <sarbole@bancolombia.com.co>
2024-06-13 15:27:38 -0500

    remove trash

--- tmp/rules/cloudformation/EC2/SecurityGroupUnrestrictedIngress3389.py
+++
@@ -1,9 +0,0 @@
-from AbsSecurityGroupUnrestrictedIngressCustom import \
-    AbsSecurityGroupUnrestrictedIngressCustom
-
-class SecurityGroupUnrestrictedIngress3389(AbsSecurityGroupUnrestrictedIngressCustom):
-    def __init__(self):
-        super().__init__(check_id="CKV_AWS_261", port=3389)
-
-
-check = SecurityGroupUnrestrictedIngress3389()
