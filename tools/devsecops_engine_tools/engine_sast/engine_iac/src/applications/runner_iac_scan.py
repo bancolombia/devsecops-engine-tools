c5d73896754384b63775ca12a992c426aa731f3e
Michael David Herrera Vargas <micherre@bancolombia.com.co>
2024-06-17 09:05:08 -0500

    creation and implementation of the driven adapter for kubescape

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/applications/runner_iac_scan.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/applications/runner_iac_scan.py
@@ -4,14 +4,19 @@ from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.entry_poin
 from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.driven_adapters.checkov.checkov_tool import (
     CheckovTool
 )
+from devsecops_engine_tools.engine_sast.engine_iac.src.infrastructure.driven_adapters.kubescape.kubescape_tool import (
+    KubescapeTool
+)
 
 
 def runner_engine_iac(dict_args, tool, secret_tool, devops_platform_gateway, env):
     try:
         # Define driven adapters for gateways
         tool_gateway = None
-        if (tool == "CHECKOV"):
+        if tool == "CHECKOV":
             tool_gateway = CheckovTool()
+        elif tool == "KUBESCAPE":
+            tool_gateway = KubescapeTool()
 
         return init_engine_sast_rm(
             devops_platform_gateway=devops_platform_gateway,
