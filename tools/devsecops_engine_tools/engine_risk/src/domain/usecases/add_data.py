40a6d425259843903eecbfde9c21086c5a4e75ce
santsant_bancol <santsant@bancolombia.com.co>
2024-07-02 10:05:32 -0500

    Import epss

---
+++ tools/devsecops_engine_tools/engine_risk/src/domain/usecases/add_data.py
@@ -0,0 +1,63 @@
+import requests
+import datetime
+import io
+import gzip
+import csv
+
+from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
+from devsecops_engine_tools.engine_utilities import settings
+
+logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
+
+class AddData:
+    def __init__(
+        self,
+        findings,
+    ):
+        self.findings = findings
+
+    @staticmethod
+    def download_epss_data():
+        base_url = "https://epss.cyentia.com/epss_scores-{}.csv.gz"
+        date = datetime.datetime.now()
+        attempts = 0
+        while attempts < 7:
+            formatted_date = date.strftime('%Y-%m-%d')
+            url = base_url.format(formatted_date)
+            response = requests.get(url)
+            if response.status_code == 200:
+                with gzip.open(io.BytesIO(response.content), 'rt') as f:
+                    data = f.read()
+                logger.info(f"EPSS data downloaded for date: {formatted_date}")
+                return data
+            else:
+                date -= datetime.timedelta(days=1)
+                attempts += 1
+        print("Could not find EPSS data from de last 7 days.")
+        logger.error("Could not find EPSS data from de last 7 days.")
+        return None
+
+    def add_epss(self, findings, epss_data):
+        epss_dict = {}
+        csv_reader = csv.reader(io.StringIO(epss_data))
+        for row in csv_reader:
+            if len(row) >= 2:
+                epss_dict[row[0]] = row[1]
+
+        for finding in findings:
+            if "CVE" in finding.vul_id:
+                epss_value = epss_dict.get(finding.vul_id)
+                if epss_value:
+                    finding.epss = epss_value
+                else:
+                    finding.epss = ""
+            else:
+                finding.epss = ""
+
+
+    def add_data(self):
+        epss_data = self.download_epss_data()
+        if epss_data:
+            self.add_epss(self.findings, epss_data)
+            return self.findings
+        return None
