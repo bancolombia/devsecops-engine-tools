24283aeb1788facd48ad4e6a2162625346063053
luarredo <luarredo@bancolombia.com.co>
2024-07-11 13:59:10 -0500

    Improved code execution and flow

--- ide_extension/vscode/devsecops/src/extension.ts
+++ ide_extension/vscode/devsecops/src/extension.ts
@@ -2,17 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {exec} from 'child_process';
+import { Scanner } from './infraestructure/drivenAdapter/Scanner';
 
 function removeAnsiEscapeCodes(text: string): string {
     return text.replace(/\x1b\[[0-9;]*m/g, '');
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('DevSecOpse IDE Extension active');
 
 	// The command has been defined in the package.json file
@@ -25,41 +22,28 @@ export function activate(context: vscode.ExtensionContext) {
 		
 	});
 
-	const iacScanDisposable = vscode.commands.registerCommand('devsecops.iacScan', () => {
-
-		vscode.window.showInformationMessage('Iac Scan init');
+	const iacScanDisposable = vscode.commands.registerCommand('devsecops.iacScan', async () => {
+		const selectedFolder = await vscode.window.showOpenDialog({
+			canSelectFolders: true,
+			canSelectFiles: false,
+			canSelectMany: false,
+			openLabel: 'Select Folder'
+		});
+		if (selectedFolder && selectedFolder.length > 0) {
+			let folderPath = selectedFolder[0].fsPath;
+			
+			folderPath = folderPath.replace(/^file:\/\//, '');
 		
-		// Crea un canal de salida
-        const outputChannel = vscode.window.createOutputChannel('IaC Scan Results');
+			vscode.window.showInformationMessage(`Devsecops Scanning: ${folderPath}`);
 
-        // Ejecuta el comando 'ls' en el directorio '~/'
-		
-		exec('pwd', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            // Muestra el resultado en el canal de salida
-            outputChannel.appendLine('IaC Scan Output:');
-            outputChannel.appendLine(stdout);
-            outputChannel.show();
-        });
-
-        exec('docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ./ms_artifact/ms_artifact:/ms_artifact devsecops_docker_36  devsecops-engine-tools --platform_devops local --remote_config_repo example_remote_config_local --tool engine_iac --folder_path /ms_artifact', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            // Muestra el resultado en el canal de salida
-			const cleanedOutput = removeAnsiEscapeCodes(stdout);
-            outputChannel.appendLine('IaC Scan Output:');
-            outputChannel.appendLine(cleanedOutput);
-            outputChannel.show();
-        });
-	
+			const scanner = new Scanner();
+			const outputChannel = vscode.window.createOutputChannel('IaC Scan Results');
+			scanner.iacScan(folderPath, outputChannel);
+		}
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(iacScanDisposable);
 }
 
 // This method is called when your extension is deactivated
