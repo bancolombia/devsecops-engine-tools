19b4d7a7f7e33ae14cb5b07739390bc8d594e08e
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2024-07-15 08:36:27 -0500

    handle len files

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/domain/usecases/iac_scan.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/domain/usecases/iac_scan.py
@@ -99,8 +99,9 @@ class IacScan:
         if(dict_args["folder_path"]):
             if config_tool.update_service_file_name_cft == "True" and "cloudformation" in dict_args["platform"]:
                 files = os.listdir(os.path.join(os.getcwd(), dict_args["folder_path"]))
-                name_file, _ = os.path.splitext(files[0])
-                config_tool.scope_pipeline = f"{config_tool.scope_pipeline}_{name_file}"
+                if len(files) > 0:
+                    name_file, _ = os.path.splitext(files[0])
+                    config_tool.scope_pipeline = f"{config_tool.scope_pipeline}_{name_file}"
 
             folders_to_scan = [dict_args["folder_path"]]
         else:
