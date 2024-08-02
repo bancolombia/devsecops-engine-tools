24941a731b58d199341c7162e210d1e0300e1471
santsant_bancol <santsant@bancolombia.com.co>
2024-08-02 08:57:19 -0500

    Get epss data

--- tools/devsecops_engine_tools/engine_risk/src/domain/usecases/add_data.py
+++ tools/devsecops_engine_tools/engine_risk/src/domain/usecases/add_data.py
@@ -21,7 +21,7 @@ class AddData:
         base_url = "https://epss.cyentia.com/epss_scores-{}.csv.gz"
         date = datetime.datetime.now()
         attempts = 0
-        while attempts < 7:
+        while attempts < 2:
             formatted_date = date.strftime('%Y-%m-%d')
             url = base_url.format(formatted_date)
             response = requests.get(url)
@@ -33,23 +33,25 @@ class AddData:
             else:
                 date -= datetime.timedelta(days=1)
                 attempts += 1
-        print("Could not find EPSS data from de last 7 days.")
-        logger.error("Could not find EPSS data from de last 7 days.")
+        print("Could not find EPSS data from de last 2 days. Skipping add EPS data...")
+        logger.error("Could not find EPSS data from de last 2 days. Skipping add EPS data...")
         return None
 
-    def add_epss(self, findings, epss_data):
+    def get_epss_dict(self, epss_data):
         epss_dict = {}
         csv_reader = csv.reader(io.StringIO(epss_data))
         for row in csv_reader:
             if len(row) >= 2:
                 epss_dict[row[0]] = row[1]
-
-        for finding in findings:
-            finding.epss = epss_dict.get(finding.vul_id, None)
+        return epss_dict
 
     def add_data(self):
-        epss_data = self.download_epss_data()
-        if epss_data:
-            self.add_epss(self.findings, epss_data)
-            return self.findings
-        return None
+        needs_epss_update = any(finding.vul_id[:3] == "CVE" and finding.epss_score == 0 for finding in self.findings)
+        if needs_epss_update:
+            epss_data = self.download_epss_data()
+            if epss_data:
+                epss_dict = self.get_epss_dict(epss_data)
+                for finding in self.findings:
+                    if finding.vul_id[:3] == "CVE" and finding.epss_score == 0:
+                        finding.epss_score = epss_dict.get(finding.vul_id, 0)
+        return self.findings
--- tools/devsecops_engine_tools/engine_risk/src/domain/usecases/handle_filters.py
+++ tools/devsecops_engine_tools/engine_risk/src/domain/usecases/handle_filters.py
@@ -1,17 +1,10 @@
 class HandleFilters:
-    def __init__(
-        self,
-        remote_config,
-    ):
-        self.remote_config = remote_config
 
     def filter(self, findings):
-        tag_list = self.remote_config["TAG_FILTER"]
         severity_list = ["critical", "high", "medium", "low"]
         return list(
             filter(
                 lambda finding: finding.active
-                and any(tag in finding.tags for tag in tag_list)
                 and (finding.severity.lower() in severity_list),
                 findings,
             )
