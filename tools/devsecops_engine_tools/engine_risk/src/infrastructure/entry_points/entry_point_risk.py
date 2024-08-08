7027ec54f7298887dfa11d72ba28ae54d76adc0f
santsant_bancol <santsant@bancolombia.com.co>
2024-08-08 16:00:03 -0500

    feat(engine_risk): :bug: Skip task from ConfigTool.json "IGNORE_ANALYSIS_PATTERN" and from Exclusions.json "SKIP_TOOL"

--- tools/devsecops_engine_tools/engine_risk/src/infrastructure/entry_points/entry_point_risk.py
+++ tools/devsecops_engine_tools/engine_risk/src/infrastructure/entry_points/entry_point_risk.py
@@ -8,6 +8,8 @@ from devsecops_engine_tools.engine_risk.src.domain.usecases.add_data import (
     AddData,
 )
 
+import re
+
 from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
 from devsecops_engine_tools.engine_utilities import settings
 
@@ -18,20 +20,42 @@ def init_engine_risk(devops_platform_gateway, print_table_gateway, dict_args, fi
     remote_config = devops_platform_gateway.get_remote_config(
         dict_args["remote_config_repo"], "engine_risk/ConfigTool.json"
     )
-    if findings:
-        handle_filters = HandleFilters(remote_config)
-        active_findings = handle_filters.filter(findings)
-        if active_findings:
-            data_added = AddData(active_findings).add_data()
-            BreakBuild(
-                devops_platform_gateway, print_table_gateway, remote_config
-            ).process(
-                data_added,
-            )
-
-        else:
-            print("No active findings found in Vulnerability Management Platform")
-            logger.info("No active findings found in Vulnerability Management Platform")
+    exclusions = devops_platform_gateway.get_remote_config(
+        dict_args["remote_config_repo"], "engine_risk/Exclusions.json"
+    )
+    pipeline_name = devops_platform_gateway.get_variable("pipeline_name")
+    if should_skip_analysis(remote_config, pipeline_name, exclusions):
+        print("Tool skipped by DevSecOps Policy.")
+        logger.info("Tool skipped by DevSecOps Policy.")
     else:
+        process_findings(
+            findings, remote_config, devops_platform_gateway, print_table_gateway
+        )
+
+
+def should_skip_analysis(remote_config, pipeline_name, exclusions):
+    ignore_pattern = remote_config["IGNORE_ANALYSIS_PATTERN"]
+    return re.match(ignore_pattern, pipeline_name, re.IGNORECASE) or (
+        pipeline_name in exclusions and exclusions[pipeline_name].get("SKIP_TOOL", 0)
+    )
+
+
+def process_findings(
+    findings, remote_config, devops_platform_gateway, print_table_gateway
+):
+    if not findings:
         print("No findings found in Vulnerability Management Platform")
         logger.info("No findings found in Vulnerability Management Platform")
+        return
+
+    handle_filters = HandleFilters(remote_config)
+    active_findings = handle_filters.filter(findings)
+    if not active_findings:
+        print("No active findings found in Vulnerability Management Platform")
+        logger.info("No active findings found in Vulnerability Management Platform")
+        return
+
+    data_added = AddData(active_findings).add_data()
+    BreakBuild(devops_platform_gateway, print_table_gateway, remote_config).process(
+        data_added
+    )
--- tools/devsecops_engine_tools/engine_risk/test/infrastructure/entry_points/test_entry_point_risk.py
+++ tools/devsecops_engine_tools/engine_risk/test/infrastructure/entry_points/test_entry_point_risk.py
@@ -1,47 +1,90 @@
 from unittest.mock import MagicMock, patch
 from devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk import (
     init_engine_risk,
+    should_skip_analysis,
+    process_findings,
 )
 
 
 @patch(
+    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.should_skip_analysis"
+)
+@patch("builtins.print")
+def test_init_engine_risk_skip(mock_print, mock_skip):
+    dict_args = {"remote_config_repo": "remote_config"}
+    findings = ["finding1", "finding2"]
+    mock_skip.return_value = True
+
+    init_engine_risk(MagicMock(), MagicMock(), dict_args, findings)
+
+    mock_print.assert_called_once_with("Tool skipped by DevSecOps Policy.")
+
+
+@patch(
+    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.should_skip_analysis"
+)
+@patch(
+    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.process_findings"
+)
+def test_init_engine_risk_process(mock_process, mock_skip):
+    dict_args = {"remote_config_repo": "remote_config"}
+    findings = ["finding1", "finding2"]
+    mock_skip.return_value = False
+
+    init_engine_risk(MagicMock(), MagicMock(), dict_args, findings)
+
+    mock_process.assert_called_once()
+
+
+def test_should_skip_analysis():
+    remote_config = {"IGNORE_ANALYSIS_PATTERN": "pattern"}
+    pipeline_name = "pipeline"
+    exclusions = {"pipeline": {"SKIP_TOOL": 1}}
+
+    assert should_skip_analysis(remote_config, pipeline_name, exclusions) == True
+
+
+@patch("builtins.print")
+def test_process_findings_no_findings(mock_print):
+    findings = []
+
+    process_findings(findings, MagicMock(), MagicMock(), MagicMock())
+
+    mock_print.assert_called_once_with(
+        "No findings found in Vulnerability Management Platform"
+    )
+
+
+@patch("builtins.print")
+@patch(
     "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.HandleFilters"
+)
+def test_process_findings_no_active_findings(mock_filters, mock_print):
+    findings = ["finding1", "finding2"]
+    mock_filters.return_value.filter.return_value = []
+
+    process_findings(findings, MagicMock(), MagicMock(), MagicMock())
+
+    mock_print.assert_called_once_with(
+        "No active findings found in Vulnerability Management Platform"
+    )
+
+
+@patch(
+    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.HandleFilters"
+)
+@patch(
+    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.AddData"
 )
 @patch(
     "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.BreakBuild"
 )
-def test_init_engine_risk(mock_break_build, mock_handle_filters):
-    dict_args = {"remote_config_repo": "remote_config"}
+def test_process_findings(mock_break, mock_add, mock_filters):
     findings = ["finding1", "finding2"]
+    active_findings = ["active1", "active2"]
+    mock_filters.return_value.filter.return_value = active_findings
 
-    init_engine_risk(MagicMock(), MagicMock(), dict_args, findings)
+    process_findings(findings, MagicMock(), MagicMock(), MagicMock())
 
-    mock_handle_filters.assert_called_once()
-    mock_break_build.assert_called_once()
-
-
-@patch(
-    "devsecops_engine_tools.engine_risk.src.infrastructure.entry_points.entry_point_risk.HandleFilters"
-)
-@patch("builtins.print")
-def test_init_engine_risk_no_active_findigs(mock_print, mock_handle_filters):
-    dict_args = {"remote_config_repo": "remote_config"}
-    findings = ["finding"]
-
-    mock_handle_filters.filter.return_value = []
-
-    init_engine_risk(MagicMock(), MagicMock(), dict_args, findings)
-
-    mock_print.assert_called_once()
-
-
-@patch("builtins.print")
-def test_init_engine_risk_no_findigs(mock_print):
-    dict_args = {"remote_config_repo": "remote_config"}
-    findings = []
-
-    init_engine_risk(MagicMock(), MagicMock(), dict_args, findings)
-
-    mock_print.assert_called_with(
-        "No findings found in Vulnerability Management Platform"
-    )
+    mock_add.assert_called_once_with(active_findings)
+    mock_break.assert_called_once()
