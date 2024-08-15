4bc93d39d9719b427092ed7a667dd495dff8309a
santsant_bancol <santsant@bancolombia.com.co>
2024-08-15 11:17:49 -0500

    test(engine_risk): :memo: Fix unit tests

--- tools/devsecops_engine_tools/engine_risk/test/domain/usecases/test_handle_filters.py
+++ tools/devsecops_engine_tools/engine_risk/test/domain/usecases/test_handle_filters.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock, patch
 from devsecops_engine_tools.engine_risk.src.domain.usecases.handle_filters import (
     HandleFilters,
 )
@@ -7,10 +8,9 @@ from devsecops_engine_tools.engine_core.src.domain.model.report import Report
 
 class TestHandleFilters(unittest.TestCase):
     def setUp(self):
-        self.remote_config = {"SEVERITY_LIST": ["low"]}
         self.findings = [
             Report(
-                id="id2",
+                id=[{"vulnerability_id": "CVE-2021-1234"}],
                 date="21022024",
                 status="stat2",
                 where="path",
@@ -19,7 +19,7 @@ class TestHandleFilters(unittest.TestCase):
                 active=True,
             ),
             Report(
-                id="id2",
+                id=[{"vulnerability_id": "CVE-2021-1234"}],
                 date="21022024",
                 status="stat2",
                 where="path2",
@@ -28,7 +28,7 @@ class TestHandleFilters(unittest.TestCase):
                 active=None,
             ),
             Report(
-                id="id3",
+                id=[{"vulnerability_id": "vuln_id"}],
                 date="21022024",
                 status="stat3",
                 where="path3",
@@ -37,9 +37,27 @@ class TestHandleFilters(unittest.TestCase):
                 active=True,
             ),
         ]
-        self.handle_filters = HandleFilters(self.remote_config)
+        self.handle_filters = HandleFilters()
 
-    def test_filter(self):
+    @patch(
+        "devsecops_engine_tools.engine_risk.src.domain.usecases.handle_filters.HandleFilters._get_active_findings"
+    )
+    @patch(
+        "devsecops_engine_tools.engine_risk.src.domain.usecases.handle_filters.HandleFilters._get_priority_vulnerability"
+    )
+    def test_filter(self, mock_get_priority_vulnerability, mock_get_active_findings):
+        mock_get_active_findings.return_value = ["Finding1", "Finding2"]
+
         result = self.handle_filters.filter(self.findings)
 
-        assert len(result) == 1
+        assert len(result) == 2
+    
+    def test__get_active_findings(self):
+        result = self.handle_filters._get_active_findings(self.findings)
+
+        assert len(result) == 2
+
+    def test__get_priority_vulnerability(self):
+        result = self.handle_filters._get_priority_vulnerability(self.findings)
+
+        assert len(result) == 3
