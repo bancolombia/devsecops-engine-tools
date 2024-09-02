d5cb0c8158dc99f454d0cca238773402bfd94a81
luarredo <luarredo@bancolombia.com.co>
2024-09-02 10:13:52 -0500

    chore: Update devsecops scanner Docker image version to 1.8.7

--- ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/Scanner.ts
+++ ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/Scanner.ts
@@ -6,17 +6,18 @@ import OutputManager from "../helper/OutputManager";
 export class Scanner implements IScannerGateway{
 
     iacScan(folderToScan: string, outputChannel: OutputChannel): void {
-        exec(`docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${folderToScan}:/ms_artifact devsecops_docker_36  devsecops-engine-tools --platform_devops local --remote_config_repo example_remote_config_local --tool engine_iac --folder_path /ms_artifact`, (error, stdout, stderr) => {
+        exec(`docker run --rm -v ${folderToScan}:/ms_artifact bancolombia/devsecops-engine-tools:1.8.7  devsecops-engine-tools --platform_devops local --remote_config_repo docker_default_remote_config --tool engine_iac --folder_path /ms_artifact`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
+                console.error(`stderr: ${stderr}`);
                 return;
             }
-            // Muestra el resultado en el canal de salida
-			const cleanedOutput = OutputManager.removeAnsiEscapeCodes(stdout);
+
+            const cleanedOutput = OutputManager.removeAnsiEscapeCodes(stdout);
             outputChannel.appendLine('IaC Scan Output:');
             outputChannel.appendLine(cleanedOutput);
             outputChannel.show();
         });
     }
-    
+
 }
