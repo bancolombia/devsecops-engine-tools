541ee96e9daa51c545390b0093a06fd4b54a63c1
russbelln <rnorena@bancolombia.com.co>
2024-09-06 17:28:22 -0500

    adjust OAuth adapter

--- tools/devsecops_engine_tools/engine_dast/src/infrastructure/driven_adapters/oauth/generic_oauth.py
+++ tools/devsecops_engine_tools/engine_dast/src/infrastructure/driven_adapters/oauth/generic_oauth.py
@@ -4,30 +4,31 @@ from devsecops_engine_tools.engine_dast.src.domain.model.gateways.authentication
 )
 
 class GenericOauth(AuthenticationGateway):
-    def __init__(self, data):
+    def __init__(self, data, endpoint):
         self.data: dict = data
+        self.endpoint = endpoint
         self.config = {}
 
     def process_data(self):
 
         self.config = {
-            "client_id": self.data["security_auth"]["client_id"],
+            "method": self.data["security_auth"].get("method", "POST"),
+            "path": self.data["security_auth"]["path"],
+            "grant_type": self.data["security_auth"]["grant_type"],
+            "scope": self.data["security_auth"].get("scope", None),
+            "headers": self.data["security_auth"]["headers"],
             "client_secret": self.data["security_auth"]["client_secret"],
-            "endpoint": self.data["security_auth"]["endpoint"],
-            "username": self.data["security_auth"].get("username"),
-            "password": self.data["security_auth"].get("password"),
-            "scope": self.data["security_auth"].get("scope")
+            "client_id": self.data["security_auth"]["client_id"]
         }
-
         return self.config
 
     def get_access_token(self):
         auth_config = self.process_data()
 
-        if auth_config["username"] and auth_config["password"]:
+        if auth_config["grant_type"].lower() "client_credentials":
             return self.get_access_token_resource_owner()
         else:
-            return self.get_access_token_client_credentials()
+            raise ValueError("OAuth: Grant type is not supported yet")
 
     def get_credentials(self):
         return self.get_access_token()
@@ -35,74 +36,30 @@ class GenericOauth(AuthenticationGateway):
     def get_access_token_client_credentials(self):
         """Obtain access token using client credentials flow."""
         try:
-            required_keys = ["client_id", "client_secret", "tenant_id"]
+            required_keys = ["client_id", "client_secret"]
             if not all(key in self.config for key in required_keys):
                 raise ValueError("One or more keys is missing in OAuth config")
 
             data = {
                 "client_id": self.config["client_id"],
                 "client_secret": self.config["client_secret"],
-                "tenant_id": self.config["tenant_id"],
                 "grant_type": "client_credentials",
                 "scope": self.config["scope"],
             }
 
-            url = self.config["endpoint"]
-            headers = {
-                "Content-Type": "application/x-www-form-urlencoded",
-            }
+            url = self.endpoint + self.config["path"]
+            headers = self.config["headers"]
             response = requests.request(
-                "POST", url, headers=headers, data=data, timeout=5
+                self.config["method"], url, headers=headers, data=data, timeout=5
             )
             if 200 <= response.status_code < 300:
                 result = response.json()["access_token"]
-                return result
+                return (,f"Bearer {result}")
             else:
                 print(
-                    "Can't obtain access token"
+                    "OAuth: Can't obtain access token"
                     "token Unknown status "
                     "code {0}: -> {1}".format(response.status_code, response.text)
                 )
         except (ConnectionError, ValueError, KeyError) as e:
-            print("Can't obtain accesstoken: {0}".format(e))
-
-    def get_access_token_resource_owner(self):
-        """Obtain access token using resource owner flow."""
-        try:
-            required_keys = [
-                "client_id",
-                "client_secret",
-                "tenant_id",
-                "username",
-                "password"
-            ]
-            if not all(key in self.config for key in required_keys):
-                raise ValueError("Falta una o más claves de configuración.")
-
-            url = self.config["endpoint"]
-            data = {
-                "client_id": self.config["client_id"],
-                "client_secret": self.config["client_secret"],
-                "grant_type": "password",
-                "scope": self.config["scope"],
-                "username": self.config["username"],
-                "password": self.config["password"],
-            }
-
-            headers = {
-                "Content-Type": "application/x-www-form-urlencoded",
-            }
-            response = requests.request(
-                "POST", url, headers=headers, data=data, timeout=5
-            )
-            if 200 <= response.status_code < 300:
-                result = response.json()["access_token"]
-                return result
-            else:
-                print(
-                    "[graph] No se obtuvo el access "
-                    "token Unknown status "
-                    "code {0}: -> {1}".format(response.status_code, response.text)
-                )
-        except (ConnectionError, ValueError, KeyError) as e:
-            print("[graph] No se obtuvo el access " "token Excepcion: {0}".format(e))
+            print("OAuth: Can't obtain access token: {0}".format(e))
