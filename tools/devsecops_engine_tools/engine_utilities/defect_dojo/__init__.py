26b391aa2d715fcb6a1fca0de1063c05d03e46ab
Santiago Santa <santiagosantaa9@gmail.com>
2024-09-16 17:02:42 -0500

    feat(engine_risk): :rocket: Add coverage for vulnerability management platform services

--- tools/devsecops_engine_tools/engine_utilities/defect_dojo/__init__.py
+++ tools/devsecops_engine_tools/engine_utilities/defect_dojo/__init__.py
@@ -3,3 +3,4 @@ from .domain.request_objects.finding import FindingRequest
 from .applications.defect_dojo import DefectDojo
 from .applications.finding import Finding
 from .applications.connect import Connect
+from .applications.engagement import Engagement
