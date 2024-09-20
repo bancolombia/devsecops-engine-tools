d5884a34a986f681580a045a629692ff6eef65f1
Carlos Javier Lopez Ortega <carloslo@bancolombia.com.co>
2024-09-20 11:30:50 -0500

    deploy dev

--- tools/devsecops_engine_tools/engine_sast/engine_secret/src/domain/model/gateway/git_gateway.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/src/domain/model/gateway/git_gateway.py
@@ -5,11 +5,6 @@ class GitGateway(metaclass=ABCMeta):
     @abstractmethod
     def get_files_pull_request(self, sys_working_dir: str,
                                target_branch: str,
-                               config_target_branch: dict,
-                               source_branch: str,
-                               access_token: str,
-                               collection_uri: str,
-                               team_project: str,
-                               repository_name: str,
-                               repository_provider: str) -> dict:
+                               source_branch: str
+                               ) -> dict:
         "get_files_pull_request"
