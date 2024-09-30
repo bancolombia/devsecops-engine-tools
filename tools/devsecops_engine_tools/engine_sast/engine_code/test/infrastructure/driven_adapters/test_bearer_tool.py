ff7fe58cb26eeeee0908b10a6641327d9af05c58
jcamilomolinar <jucmolin@bancolombia.com.co>
2024-09-30 15:02:55 -0500

    fix: copy files in another folder to scan to improve execution time and fix tests

--- tools/devsecops_engine_tools/engine_sast/engine_code/test/infrastructure/driven_adapters/test_bearer_tool.py
+++ tools/devsecops_engine_tools/engine_sast/engine_code/test/infrastructure/driven_adapters/test_bearer_tool.py
@@ -1,6 +1,7 @@
 import unittest
-from unittest.mock import patch, Mock, call
+from unittest.mock import patch, Mock, call, MagicMock, mock_open, ANY
 import subprocess
+import os
 from devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool import (
     BearerTool
 )
@@ -12,18 +13,22 @@ class TestBearerTool(unittest.TestCase):
     )
     def test_install_tool_command_not_found(self, mock_subprocess):
         # Arrange
-        mock_subprocess.return_value = Mock(stderr=b"bearer: command not found", decode=lambda: "bearer: command not found")
+        mock_subprocess.side_effect = [
+            MagicMock(returncode=1),  # "bearer version" fails
+            MagicMock(returncode=1),  # first try of instalation fails
+            MagicMock(returncode=0),  # second try success
+         ]
         tool = BearerTool()
-        agent_work_folder = "/agent/work/folder"
 
         # Act
-        tool.install_tool(agent_work_folder)
+        tool.install_tool()
         
         # Assert
-        install_command = f"curl -sfL https://raw.githubusercontent.com/Bearer/bearer/main/contrib/install.sh | sh -s -- -b {agent_work_folder}/bin"
+        install_command = f"curl -sfL https://raw.githubusercontent.com/Bearer/bearer/main/contrib/install.sh | sh -s -- -b ."
         mock_subprocess.assert_has_calls([
-            call(f"{agent_work_folder}/bin/bearer version", capture_output=True, shell=True),
-            call(install_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            call(f"./bearer version", capture_output=True, shell=True),
+            call(install_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True),
+            call(install_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True),
         ])
 
     @patch(
@@ -31,15 +36,14 @@ class TestBearerTool(unittest.TestCase):
     )
     def test_install_tool_already_installed(self, mock_subprocess):
         # Arrange
-        mock_subprocess.return_value = Mock(stderr=b"", decode=lambda: "")
+        mock_subprocess.return_value = Mock(returncode=0)
         tool = BearerTool()
-        agent_work_folder = "/agent/work/folder"
 
         # Act
-        tool.install_tool(agent_work_folder)
+        tool.install_tool()
         
         # Assert
-        mock_subprocess.assert_called_once_with(f"{agent_work_folder}/bin/bearer version", capture_output=True, shell=True)
+        mock_subprocess.assert_called_once_with("./bearer version", capture_output=True, shell=True)
 
     def test_config_data(self):
         # Arrange
@@ -94,121 +98,205 @@ class TestBearerTool(unittest.TestCase):
             default_flow_style=False
         )
 
-    def test_skip_rules_list(self):
+    @patch(
+        "os.makedirs"
+    )
+    @patch(
+        "shutil.copy2"
+    )
+    def test_copy_file(self, mock_copy2, mock_makedirs):
         # Arrange
+        pull_file = "file1.txt"
+        agent_work_folder = "/agent/work/folder"
+        repository = "my_repo"
+        path_to_scan = "/agent/work/folder/copy_folder"
         tool = BearerTool()
-        list_exclusions = [Mock(where="file1", id="vul_id1"), Mock(where="all", id="vul_id2"), Mock(where="file2", id="vul_id3")]
-        pull_file = "file1"
-        
+
         # Act
-        result = tool.skip_rules_list(list_exclusions, pull_file)
+        tool.copy_file(pull_file, agent_work_folder, repository, path_to_scan)
+
+        expected_destination_dir = os.path.join(path_to_scan, f"{repository}")
+        mock_makedirs.assert_called_once_with(expected_destination_dir, exist_ok=True)
+
+        expected_source_path = f"{agent_work_folder}/{repository}/{pull_file}"
+        expected_destination_path = os.path.join(path_to_scan, f"{repository}/{pull_file}")
 
         # Assert
-        self.assertEqual(result, ["vul_id1", "vul_id2"])
-    
+        mock_copy2.assert_called_once_with(expected_source_path, expected_destination_path)
+
     @patch(
         "subprocess.run"
     )
     @patch(
         "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerDeserealizator.get_list_finding"
     )
-    @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerTool.install_tool"
-    )
-    @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerTool.create_config_file"
-    )
-    def test_run_tool_with_folder_to_scan(self, mock_create_config_file, mock_install_tool, mock_get_list_finding, mock_subprocess):
+    def test_scan_path(self, mock_get_list_finding, mock_run):
         # Arrange
-        tool = BearerTool()
-        pull_request_files, list_exclusions = [], []
-        folder_to_scan = "/path/to/scan"
-        agent_work_folder = "/agent/work/folder"
-        repository = "test_repo"
-        list_rules = ["rule1", "rule2"]
+        mock_run.return_value = MagicMock(returncode=0)
         mock_get_list_finding.return_value = ["finding1", "finding2"]
+        path = "/path/to/scan/file.js"
+        agent_work_folder = "/agent/work/folder"
+        tool = BearerTool()
 
         # Act
-        findings, path_file_results = tool.run_tool(
-            folder_to_scan,
-            pull_request_files,
-            agent_work_folder,
-            repository,
-            list_exclusions,
-            list_rules
-        )
+        findings = tool.scan_path(path, agent_work_folder)
 
         # Assert
-        mock_install_tool.assert_called_once_with(agent_work_folder)
-        mock_create_config_file.assert_called_once_with(agent_work_folder)
-        expected_command = f"{agent_work_folder}/bin/bearer scan {folder_to_scan} --config-file {agent_work_folder}/bearer.yml"
-        mock_subprocess.assert_called_once_with(expected_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        mock_run.assert_called_once_with(
+            f"./bearer scan {path} --config-file {agent_work_folder}/bearer.yml",
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True
+        )
         mock_get_list_finding.assert_called_once_with(f"{agent_work_folder}/bearer-scan.json", agent_work_folder)
         self.assertEqual(findings, ["finding1", "finding2"])
-        self.assertEqual(path_file_results, f"{agent_work_folder}/bearer-scan.json")
     
     @patch(
-        "subprocess.run"
+        "builtins.open",
+        new_callable=mock_open
     )
     @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerDeserealizator.get_list_finding"
+        "json.load"
     )
     @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerTool.install_tool"
+        "json.dump"
     )
-    @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerTool.create_config_file"
-    )
-    @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerScanFileMaker.add_vulnerabilities"
-    )
-    @patch(
-        "devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.bearer.bearer_tool.BearerScanFileMaker.make_scan_file"
-    )
-    def test_run_tool_with_pull_request_files(self,mock_bearer_scan_file_maker_create, mock_bearer_scan_file_maker_add, mock_create_config_file, mock_install_tool, mock_get_list_finding, mock_subprocess):
+    def test_format_scan_file(self, mock_json_dump, mock_json_load, mock_file_open):
         # Arrange
-        tool = BearerTool()
-        folder_to_scan = None
-        pull_request_files = ["file1.js", "file2.js"]
+        mock_json_load.return_value = {
+            "high": [{"name": "vul1"}]
+        }
+        scan_result_path = "/path/to/scan_result.json"
         agent_work_folder = "/agent/work/folder"
-        repository = "test_repo"
-        list_exclusions = [Mock(where="file_test.js", id="vul_id1")]
-        list_rules = ["rule1", "rule2"]
-        mock_get_list_finding.side_effect = [["finding1"], ["finding2"]]
+        tool = BearerTool()
+        mock_file_open.side_effect = [
+            mock_open(read_data='{"high": [{"name": "vul1"}]}').return_value,
+            mock_open().return_value
+        ]
 
-        # Act
-        findings, _ = tool.run_tool(
-            folder_to_scan,
-            pull_request_files,
-            agent_work_folder,
-            repository,
-            list_exclusions,
-            list_rules
-        )
+        #Act
+        result_path = tool.format_scan_file(scan_result_path, agent_work_folder)
 
         # Assert
-        mock_install_tool.assert_called_once_with(agent_work_folder)
-        mock_create_config_file.assert_has_calls([
-                call(agent_work_folder, list_skip_rules=tool.skip_rules_list(list_exclusions, "file1.js")),
-                call(agent_work_folder, list_skip_rules=tool.skip_rules_list(list_exclusions, "file2.js"))
-            ]     
+        expected_path = f"{agent_work_folder}/bearer-scan-vul-man.json"
+        mock_file_open.assert_any_call(scan_result_path, encoding="utf-8")
+        mock_file_open.assert_any_call(expected_path, "w")
+        mock_json_load.assert_called_once()
+        expected_data = {"high": [{"name": "vul1", "snippet": ""}]}
+        mock_json_dump.assert_called_once_with(expected_data, ANY)
+        self.assertEqual(result_path, expected_path)
+
+    @patch.object(
+        BearerTool, 
+        "install_tool"
+    )
+    @patch.object(
+        BearerTool, 
+        "create_config_file"
+    )
+    @patch.object(
+        BearerTool, 
+        "scan_path"
+    )
+    @patch.object(
+        BearerTool, 
+        "format_scan_file"
+    )
+    def test_run_tool_with_folder_to_scan(self, mock_format_scan_file, mock_scan_path, mock_create_config_file, mock_install_tool):
+        # Arrange
+        tool = BearerTool()
+        mock_scan_path.return_value = "findings_data"
+        mock_format_scan_file.return_value = "formatted_scan"
+        folder_to_scan = "/test/folder"
+        pull_request_files = ["file1.txt", "file2.txt"]
+        agent_work_folder = "/agent/work/folder"
+        repository = "repo"
+        config_tool = MagicMock()
+        config_tool.data = {
+            tool.BEARER_TOOL: {
+                "EXCLUDED_RULES": ["rule1", "rule2"],
+                "NUMBER_THREADS": 4
+            }
+        }
+        
+        # Act
+        findings, scan_result_path_formatted = tool.run_tool(
+            folder_to_scan, pull_request_files, agent_work_folder, repository, config_tool
         )
-        expected_command_file1 = f"{agent_work_folder}/bin/bearer scan {agent_work_folder}/{repository}/file1.js --config-file {agent_work_folder}/bearer.yml"
-        expected_command_file2 = f"{agent_work_folder}/bin/bearer scan {agent_work_folder}/{repository}/file2.js --config-file {agent_work_folder}/bearer.yml"
-        mock_subprocess.assert_has_calls([
-                call(expected_command_file1, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True),
-                call(expected_command_file2, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            ]     
+        
+        # Assert
+        mock_install_tool.assert_called_once()
+        mock_create_config_file.assert_called_once_with(agent_work_folder, ["rule1", "rule2"])
+        mock_scan_path.assert_called_once_with(folder_to_scan, agent_work_folder)
+        mock_format_scan_file.assert_called_once_with(f"/agent/work/folder/bearer-scan.json", agent_work_folder)
+        
+        self.assertEqual(findings, "findings_data")
+        self.assertEqual(scan_result_path_formatted, "formatted_scan")
+    
+    @patch(
+        "os.makedirs"
+    )
+    @patch(
+        "concurrent.futures.ThreadPoolExecutor"
+    )
+    @patch.object(
+        BearerTool, 
+        "install_tool"
+    )
+    @patch.object(
+        BearerTool, 
+        "create_config_file"
+    )
+    @patch.object(
+        BearerTool, 
+        "copy_file"
+    )
+    @patch.object(
+        BearerTool, 
+        "scan_path"
+    )
+    @patch.object(
+        BearerTool, 
+        "format_scan_file"
+    )
+    def test_run_tool_without_folder(self, mock_format_scan_file, mock_scan_path, mock_copy_file, mock_create_config_file, 
+                                     mock_install_tool, mock_thread_pool, mock_makedirs):
+        # Arrange
+        tool = BearerTool()
+        mock_scan_path.return_value = "findings_data"
+        mock_format_scan_file.return_value = "formatted_scan"
+        mock_executor = MagicMock()
+        mock_future = MagicMock()
+        mock_executor.submit.return_value = mock_future
+        mock_future.result.return_value = None
+        mock_thread_pool.return_value.__enter__.return_value = mock_executor
+        folder_to_scan = None
+        pull_request_files = ["file1.txt", "file2.txt"]
+        agent_work_folder = "/agent/work/folder"
+        repository = "repo"
+        config_tool = MagicMock()
+        config_tool.data = {
+            tool.BEARER_TOOL: {
+                "EXCLUDED_RULES": ["rule1", "rule2"],
+                "NUMBER_THREADS": 4
+            }
+        }
+        
+        # Act
+        findings, scan_result_path_formatted = tool.run_tool(
+            folder_to_scan, pull_request_files, agent_work_folder, repository, config_tool
         )
-        mock_bearer_scan_file_maker_add.assert_has_calls([
-                call(f"{agent_work_folder}/bearer-scan.json"),
-                call(f"{agent_work_folder}/bearer-scan.json")
-            ]     
-        )
-        mock_bearer_scan_file_maker_create.assert_called_once_with(agent_work_folder)
-        mock_get_list_finding.assert_has_calls([
-                call(f"{agent_work_folder}/bearer-scan.json", agent_work_folder, list_rules=list_rules),
-                call(f"{agent_work_folder}/bearer-scan.json", agent_work_folder, list_rules=list_rules)
-            ]     
-        )
-        self.assertEqual(findings, ["finding1", "finding2"])
+        
+        # Assert
+        folder_copy_files = f"{agent_work_folder}/copy_files_bearer"
+        mock_install_tool.assert_called_once()
+        mock_create_config_file.assert_called_once_with(agent_work_folder, ["rule1", "rule2"])
+        mock_makedirs.assert_called_once_with(folder_copy_files, exist_ok=True)
+        mock_executor.submit.assert_any_call(tool.copy_file, "file1.txt", agent_work_folder, repository, folder_copy_files)
+        mock_executor.submit.assert_any_call(tool.copy_file, "file2.txt", agent_work_folder, repository, folder_copy_files)
+        mock_scan_path.assert_called_once_with(folder_copy_files, agent_work_folder)
+        mock_format_scan_file.assert_called_once_with(f"/agent/work/folder/bearer-scan.json", agent_work_folder)
+
+        self.assertEqual(findings, "findings_data")
+        self.assertEqual(scan_result_path_formatted, "formatted_scan")
