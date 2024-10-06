cda4b7046ff02a7710de1e220e0c054063715399
luarredo <luarredo@bancolombia.com.co>
2024-10-06 18:17:52 -0500

    Update remote repo

--- ide_extension/vscode/devsecops/src/application/InitEngineCore.ts
+++ ide_extension/vscode/devsecops/src/application/InitEngineCore.ts
@@ -2,8 +2,15 @@ import { IacScanner } from "../infraestructure/drivenAdapter/IacScanner";
 import { IacScanRequest } from "../infraestructure/entryPoint/IacScanRequest";
 import { IacScanUseCase } from "../domain/usecase/IacScanUseCase";
 import { RestClient } from "../infraestructure/drivenAdapter/RestClient";
+import { SecretScanUseCase } from "../domain/usecase/SecretScanUseCase";
+import { SecretScanRequest } from "../infraestructure/entryPoint/SecretScanRequest";
 
 export function iacScanRequest(): IacScanRequest {
     const iacScanUseCase = new IacScanUseCase(new IacScanner(), new RestClient());
     return new IacScanRequest(iacScanUseCase);
+}
+
+export function secretScanRequest(): SecretScanRequest {
+    const secretScanUseCase = new SecretScanUseCase(new IacScanner(), new RestClient());
+    return new SecretScanRequest(secretScanUseCase);
 }
