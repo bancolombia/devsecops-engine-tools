d63e30bd47f35be2319b37a9ad944c5cecf487ea
russbelln <rnorena@bancolombia.com.co>
2024-10-09 07:49:33 -0500

    refactor: (engine_dast): :recycle: Solve token_external_checks comments

--- tools/devsecops_engine_tools/engine_dast/src/applications/runner_dast_scan.py
+++ tools/devsecops_engine_tools/engine_dast/src/applications/runner_dast_scan.py
@@ -32,65 +32,69 @@ from devsecops_engine_tools.engine_dast.src.infrastructure.helpers.json_handler 
 )
 
 def runner_engine_dast(dict_args, config_tool, secret_tool, devops_platform):
-    if config_tool["TOOL"].lower() == "nuclei": # tool_gateway is the main Tool
-        tool_run = NucleiTool()
-    extra_tools = []
-    target_config = None
+    try:
+        if config_tool["TOOL"].lower() == "nuclei": # tool_gateway is the main Tool
+            tool_run = NucleiTool()
+        extra_tools = []
+        target_config = None
 
-    # Filling operations list with adapters
-    data = load_json_file(dict_args["dast_file_path"])
+        # Filling operations list with adapters
+        data = load_json_file(dict_args["dast_file_path"])
 
-    if "operations" in data: # Api
-        operations: List = []
-        for elem in data["operations"]:
-            security_type = elem["operation"]["security_auth"]["type"].lower()
-            if security_type == "jwt":
-                operations.append(
-                    ApiOperation(
-                        elem,
-                        JwtObject(
-                            elem["operation"]["security_auth"]
-                )))
-            elif security_type == "oauth":
-                operations.append(
-                    ApiOperation(
-                        elem,
-                        GenericOauth(
-                            elem["operation"]["security_auth"],
-                            data["endpoint"]
+        if "operations" in data: # Api
+            operations: List = []
+            for elem in data["operations"]:
+                security_type = elem["operation"]["security_auth"]["type"].lower()
+                if security_type == "jwt":
+                    operations.append(
+                        ApiOperation(
+                            elem,
+                            JwtObject(
+                                elem["operation"]["security_auth"]
+                    )))
+                elif security_type == "oauth":
+                    operations.append(
+                        ApiOperation(
+                            elem,
+                            GenericOauth(
+                                elem["operation"]["security_auth"],
+                                data["endpoint"]
+                            )
                         )
                     )
-                )
-            else:
-                operations.append(
-                    ApiOperation(
-                        elem,
-                        AuthClientCredential(
-                            elem["operation"]["security_auth"]
+                else:
+                    operations.append(
+                        ApiOperation(
+                            elem,
+                            AuthClientCredential(
+                                elem["operation"]["security_auth"]
+                            )
                         )
                     )
-                )
-        data["operations"] = operations
-        target_config = ApiConfig(data)
-    elif "WA" in data: # Web Application
-        if data["data"].get["security_auth"] == "oauth":
-            authentication_gateway =  GenericOauth(
-                    data["data"]["security_auth"]
-                )
-        target_config = WaConfig(data, authentication_gateway)
-    else:
-        raise ValueError("Can't match if the target type is an api or a web application ")
+            data["operations"] = operations
+            target_config = ApiConfig(data)
+        elif "WA" in data: # Web Application
+            if data["data"].get["security_auth"] == "oauth":
+                authentication_gateway =  GenericOauth(
+                        data["data"]["security_auth"]
+                    )
+            target_config = WaConfig(data, authentication_gateway)
+        else:
+            raise ValueError("Can't match if the target type is an api or a web application ")
 
-    if any((k.lower() == "jwt") for k in config_tool["EXTRA_TOOLS"]) and \
-    any(isinstance(operation.authentication_gateway, JwtObject) for operation in data["operations"] ):
-        extra_tools.append(JwtTool(target_config))
+        if any((k.lower() == "jwt") for k in config_tool["EXTRA_TOOLS"]) and \
+        any(isinstance(operation.authentication_gateway, JwtObject) for operation in data["operations"] ):
+            extra_tools.append(JwtTool(target_config))
 
-    return init_engine_dast(
-        devops_platform_gateway=devops_platform,
-        tool_gateway=tool_run,
-        dict_args=dict_args,
-        checks_token=secret_tool["github_token"] if secret_tool else os.environ["GITHUB_TOKEN"],
-        config_tool=config_tool,
-        extra_tools=extra_tools,
-        target_data=target_config
-    )
+        return init_engine_dast(
+            devops_platform_gateway=devops_platform,
+            tool_gateway=tool_run,
+            dict_args=dict_args,
+            checks_token=secret_tool["github_token"] if secret_tool else os.environ["GITHUB_TOKEN"],
+            config_tool=config_tool,
+            extra_tools=extra_tools,
+            target_data=target_config
+        )
+    except Exception as e:
+        raise Exception(f"Error engine_dast {e}")
+        
