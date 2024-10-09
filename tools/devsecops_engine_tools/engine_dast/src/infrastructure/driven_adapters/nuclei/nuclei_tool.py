9ae017b640ccdd9cf7da9affeb56bed8f0476b55
russbelln <rnorena@bancolombia.com.co>
2024-10-09 07:49:33 -0500

    refactor(engine_dast): :recycle: Solve token_external_checks comments

--- tools/devsecops_engine_tools/engine_dast/src/infrastructure/driven_adapters/nuclei/nuclei_tool.py
+++ tools/devsecops_engine_tools/engine_dast/src/infrastructure/driven_adapters/nuclei/nuclei_tool.py
@@ -19,6 +19,10 @@ from devsecops_engine_tools.engine_dast.src.infrastructure.helpers.file_generato
 from devsecops_engine_tools.engine_utilities.github.infrastructure.github_api import (
     GithubApi
 )
+from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
+from devsecops_engine_tools.engine_utilities import settings
+
+logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
 
 
 class NucleiTool(ToolGateway):
@@ -32,11 +36,11 @@ class NucleiTool(ToolGateway):
         self.TOOL: str = "NUCLEI"
 
     def configurate_external_checks(
-        self, config_tool: ConfigTool, github_token: str, output_dir: str = "/tmp"
+        self, config_tool: ConfigTool, secret: str, output_dir: str = "/tmp"
     ):
         # Create configuration dir external checks
         if config_tool.use_external_checks_dir == "True":
-            github_api = GithubApi(github_token)
+            github_api = GithubApi(secret["github_token"])
             github_api.download_latest_release_assets(
                 config_tool.external_dir_owner,
                 config_tool.external_dir_repository,
@@ -71,20 +75,34 @@ class NucleiTool(ToolGateway):
             error = result.stderr
             if (error is not None and error != ""):
                 error = error.strip()
-                print(f"Error executing nuclei: {error}")
-
+                logger.warning(
+                    f"Error executing nuclei: {error}")
         with open(target_config.output_file, "r") as f:
             json_response = json.load(f)
         return json_response
 
-    def run_tool(self, target_data, config_tool, token):
+    def run_tool(self, 
+        target_data, 
+        config_tool,
+        secret_tool, 
+        secret_external_checks
+    ):
+        secret = None
+        if secret_tool is not None:
+            secret = secret_tool
+        elif secret_external_checks is not None:
+            secret = {
+                "github_token": (
+                    secret_external_checks.split("github")[1]
+                    if "github" in secret_external_checks
+                    else None
+                )
+            }
+
         nuclei_config = NucleiConfig(target_data)
-        checks_directory = self.configurate_external_checks(config_tool, token, "/tmp") #DATA PDN
+        checks_directory = self.configurate_external_checks(config_tool, secret, "/tmp") #DATA PDN
         nuclei_config.customize_templates(checks_directory)
         result_scans = self.execute(nuclei_config)
         nuclei_deserealizator = NucleiDesealizator()
         findings_list = nuclei_deserealizator.get_list_finding(result_scans)
-        path_file_results = generate_file_from_tool(
-            self.TOOL, result_scans, config_tool
-        )
-        return findings_list, path_file_results
+        return findings_list, nuclei_config.output_file
