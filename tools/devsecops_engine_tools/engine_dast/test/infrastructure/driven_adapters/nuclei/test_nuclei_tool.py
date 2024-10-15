36ffb77605045212c76aeea14a3ed2f2abdbb95d
russbelln <rnorena@bancolombia.com.co>
2024-10-15 18:41:06 -0500

    test(engine_dast): :white_check_mark: Adjust engine dast tests
    Update nuclei tool tests

--- tools/devsecops_engine_tools/engine_dast/test/infrastructure/driven_adapters/nuclei/test_nuclei_tool.py
+++ tools/devsecops_engine_tools/engine_dast/test/infrastructure/driven_adapters/nuclei/test_nuclei_tool.py
@@ -33,39 +33,31 @@ class TestNucleiTool(unittest.TestCase):
         self.nuclei_tool = NucleiTool(target_config=self.target_config)
 
     @patch('os.environ.get', return_value="true")
-    @patch('subprocess.run')
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": "value"}')
     @patch('json.load', return_value={"key": "value"})
-    def test_execute(self, mock_json_load, mock_open, mock_subprocess_run, mock_os_environ):
+    def test_execute(self, mock_json_load, mock_open, mock_os_environ):
         target_config = NucleiConfig(self.target_config)
         result = self.nuclei_tool.execute(target_config)
-
-        mock_subprocess_run.assert_called_once()
         mock_open.assert_called_once_with(target_config.output_file, 'r')
         mock_json_load.assert_called_once()
         self.assertEqual(result, {"key": "value"})
 
-    @patch(
-    'devsecops_engine_tools.engine_dast.src.infrastructure.driven_adapters.nuclei.nuclei_tool.NucleiConfig.customize_templates'
-    )
-    @patch(
-    'devsecops_engine_tools.engine_dast.src.infrastructure.driven_adapters.nuclei.nuclei_tool.NucleiTool.configurate_external_checks',
-    return_value="dummy_directory")
-    @patch(
-    'devsecops_engine_tools.engine_dast.src.infrastructure.driven_adapters.nuclei.nuclei_tool.NucleiTool.execute',
-    return_value={"key": "value"})
-    @patch(
-    'devsecops_engine_tools.engine_dast.src.infrastructure.driven_adapters.nuclei.nuclei_tool.NucleiDesealizator.get_list_finding',
-    return_value=[Mock()])
-    def test_run_tool(self, mock_get_list_finding,
-                      mock_execute, mock_configurate_external_checks,
-                      mock_customize_templates):
+    @patch('devsecops_engine_tools.engine_dast.src.domain.model.config_tool')
+    def test_run_tool(self, mock_config_tool):
         secret_external_checks = ("github", "dummy_token")
-        findings_list, path_file_results = self.nuclei_tool.run_tool(self.target_config, self.config_tool, self.token, secret_external_checks)
-
-        mock_configurate_external_checks.assert_called_once_with(self.config_tool, self.token, "/tmp")
-        mock_customize_templates.assert_called_once_with("dummy_directory")
-        mock_execute.assert_called_once()
-        mock_get_list_finding.assert_called_once_with({"key": "value"})
+        findings_list, path_file_results = self.nuclei_tool.run_tool(
+            self.target_config, 
+            mock_config_tool, 
+            self.token, 
+            secret_external_checks
+            )
 
         self.assertEqual(path_file_results, "result_dast_scan.json")
+
+        
+        
+
+
+# Ejecuta las pruebas
+if __name__ == "__main__":
+    unittest.main()
