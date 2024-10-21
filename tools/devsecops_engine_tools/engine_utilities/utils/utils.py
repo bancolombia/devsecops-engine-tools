81bfeaa7e2c997c97f7d519d109563851fafab0a
jcamilomolinar <jucmolin@bancolombia.com.co>
2024-10-21 14:21:14 -0500

    deploy dev

--- tools/devsecops_engine_tools/engine_utilities/utils/utils.py
+++ tools/devsecops_engine_tools/engine_utilities/utils/utils.py
@@ -7,7 +7,7 @@ class Utils:
         with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
             zip_ref.extractall(extract_path)
 
-    def encode_token_to_base64(token):
+    def encode_token_to_base64(self, token):
         token_bytes = f"{token}:".encode("utf-8")
         base64_token = base64.b64encode(token_bytes).decode("utf-8")
         return base64_token
