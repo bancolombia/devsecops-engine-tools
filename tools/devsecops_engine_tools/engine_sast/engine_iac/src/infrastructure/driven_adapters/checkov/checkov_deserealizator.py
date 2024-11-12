41b37c4e68732d27d189a79c7cc74d58036782de
jcamilomolinar <jucmolin@bancolombia.com.co>
2024-11-12 11:21:08 -0500

    deploy dev

--- tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
+++ tools/devsecops_engine_tools/engine_sast/engine_iac/src/infrastructure/driven_adapters/checkov/checkov_deserealizator.py
@@ -12,13 +12,13 @@ class CheckovDeserealizator:
     def get_list_finding(
         cls, results_scan_list: list, rules, default_severity, default_category
     ) -> "list[Finding]":
-        list_open_findings = []
 
+        list_open_findings = []
         for result in results_scan_list:
             if "failed_checks" in str(result):
                 for scan in result["results"]["failed_checks"]:
                     check_id = scan.get("check_id")
-                    if len(rules) == 0:
+                    if not rules.get(check_id):
                         description = scan.get("check_name")
                         severity = default_severity.lower()
                         category = default_category.lower()
