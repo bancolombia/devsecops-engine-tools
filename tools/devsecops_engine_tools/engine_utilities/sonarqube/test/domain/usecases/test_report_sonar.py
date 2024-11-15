81679d0ee9eaf6451cac0f90026ff7941f5d9c33
jcamilomolinar <jucmolin@bancolombia.com.co>
2024-11-15 15:28:38 -0500

    fix(report_sonar): valid source code uri for input core scope

--- tools/devsecops_engine_tools/engine_utilities/sonarqube/test/domain/usecases/test_report_sonar.py
+++ tools/devsecops_engine_tools/engine_utilities/sonarqube/test/domain/usecases/test_report_sonar.py
@@ -37,7 +37,8 @@ class TestReportSonar(unittest.TestCase):
         }
 
         mock_devops_platform_gateway.get_remote_config.return_value = {
-            "PIPELINE_COMPONENTS": {}
+            "PIPELINE_COMPONENTS": {},
+            "SCOPE_VALIDATION_REGEX": ""
         }
         
         mock_sonar_gateway.get_project_keys.return_value = ["project_key_1"]
