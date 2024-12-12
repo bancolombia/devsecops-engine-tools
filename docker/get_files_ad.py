d0dc70aad00ec6a938f15a71a05d10fb6ff85646
luarredo <luarredo@bancolombia.com.co>
2024-12-12 13:11:10 -0500

    chore: Download and save Azure DevOps folder content as JSON file
    ```

--- docker/get_files_ad.py
+++ docker/get_files_ad.py
@@ -1,42 +1,21 @@
 import requests
-import os
 import sys
+import json
 
 def download_folder_from_azure_devops(organization, project, repository, path, token, download_path):
+    print(download_path)
     url = f"https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repository}/items?scopePath={path}&recursionLevel=Full&api-version=7.1"
     headers = {"Authorization": f"Basic {token}"}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        contents = response.json()['value']
-        if isinstance(contents, list):
-            for item in contents:
-                item_path = item['path']
-                if item['gitObjectType'] == 'blob':
-                    download_file_from_azure_devops(organization, project, repository, item_path, token, download_path)
-                elif item['gitObjectType'] == 'tree':
-                    new_download_path = os.path.join(download_path, os.path.relpath(item_path, path))
-                    os.makedirs(new_download_path, exist_ok=True)
-                    download_folder_from_azure_devops(organization, project, repository, item_path, token, new_download_path)
-        else:
-            print(f"Error: Expected a directory but got a file at {path}")
+        content = str(json.dumps(response.json()))
+        file_path = "example_remote_config_local/engine_sast/engine_iac/ConfigTool.json"
+        with open(file_path, "w") as file:
+            file.write(content)
+            print(f"File '{file_path}' downloaded successfully.")
     else:
         print(f"Error downloading folder. Status code: {response.status_code}")
-        print(response.text)
-
-def download_file_from_azure_devops(organization, project, repository, path, token, download_path="."):
-    url = f"https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repository}/items?path={path}&api-version=7.1"
-    headers = {"Authorization": f"Basic {token}"}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        file_name = os.path.basename(path)
-        file_path = os.path.join(download_path, file_name)
-        with open(file_path, "wb") as file:
-            file.write(response.content)
-        print(f"File {file_name} downloaded successfully to {file_path}")
-    else:
-        print(f"Error downloading file. Status code: {response.status_code}")
         print(response.text)
 
 # Example usage
