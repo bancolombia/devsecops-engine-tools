ba588f228fcb7929a0cabb13dcb33b5bc39d1429
luarredo <luarredo@bancolombia.com.co>
2024-12-12 14:18:06 -0500

    chore: Update Dockerfile and Docker Compose to include Trivy vulnerability scanner

--- docker/remoteConfigGeneration.sh
+++ docker/remoteConfigGeneration.sh
@@ -5,4 +5,4 @@ json_file_iac="/app/docker_default_remote_config/engine_core/ConfigTool.json"
 sed -i 's/"TOOL": "CHECKOV|KUBESCAPE|KICS"/"TOOL": "CHECKOV"/' "$json_file_iac"
 
 json_file_container="/app/docker_default_remote_config/engine_core/ConfigTool.json"
-sed -i 's/"TOOL": "PRISMA|TRIVY"/"TOOL": "PRISMA"/' "$json_file_container"
+sed -i 's/"TOOL": "PRISMA|TRIVY"/"TOOL": "TRIVY"/' "$json_file_container"
