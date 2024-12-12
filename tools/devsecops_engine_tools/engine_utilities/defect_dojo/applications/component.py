7c386e51e34aab66dd3aea2e98c6233e8b64e22d
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2024-12-12 10:50:46 -0500

    refactor: code smells sonar

--- tools/devsecops_engine_tools/engine_utilities/defect_dojo/applications/component.py
+++ tools/devsecops_engine_tools/engine_utilities/defect_dojo/applications/component.py
@@ -1,7 +1,10 @@
 from devsecops_engine_tools.engine_utilities.utils.api_error import ApiError
+from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
 from devsecops_engine_tools.engine_utilities.defect_dojo.infraestructure.driver_adapters.component import ComponentRestConsumer
 from devsecops_engine_tools.engine_utilities.defect_dojo.domain.user_case.component import ComponentUserCase
+from devsecops_engine_tools.engine_utilities.settings import SETTING_LOGGER
 
+logger = MyLogger.__call__(**SETTING_LOGGER).get_logger()
 
 class Component:
 
@@ -12,6 +15,7 @@ class Component:
             uc = ComponentUserCase(rest_component)
             return uc.get(request)
         except ApiError as e:
+            logger.error(f"Error during get component: {e}")
             raise e
     
     @staticmethod
@@ -21,4 +25,5 @@ class Component:
             uc = ComponentUserCase(rest_component)
             return uc.post(request)
         except ApiError as e:
+            logger.error(f"Error during create component: {e}")
             raise e
