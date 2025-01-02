2295cd2125bc6daba50bfe68d4225d5475cdd3c8
jcamilomolinar <jucmolin@bancolombia.com.co>
2025-01-02 15:04:52 -0500

    fix(gitleaks): adjust driven adapter

--- tools/devsecops_engine_tools/engine_sast/engine_secret/src/infrastructure/driven_adapters/gitleaks/gitleaks_tool.py
+++ tools/devsecops_engine_tools/engine_sast/engine_secret/src/infrastructure/driven_adapters/gitleaks/gitleaks_tool.py
@@ -66,7 +66,7 @@ class GitleaksTool(ToolGateway):
             json.dump(combined_data, f, ensure_ascii=False, indent=4)
 
     def check_path(self, path, excluded_paths):
-        parts = path.split("/")
+        parts = path.split(os.sep)
         for part in parts:
             if part in excluded_paths: return True
         return False
