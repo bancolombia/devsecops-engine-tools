a16c8e1858db5776a7251d8486e0d7070854cdc9
Michael David Herrera Vargas <micherre@bancolombia.com.co>
2025-04-11 10:51:31 -0500

    fix(azure): fix conditional

--- tools/devsecops_engine_tools/engine_utilities/azuredevops/models/AzurePredefinedVariables.py
+++ tools/devsecops_engine_tools/engine_utilities/azuredevops/models/AzurePredefinedVariables.py
@@ -10,7 +10,7 @@ class EnvVariables:
     @staticmethod
     def get_value(env_name):
         env_var = os.environ.get(env_name)
-        if env_var is None and env_name:
+        if env_var is None:
             raise ValueError(f"La variable de entorno {env_name} no está definida")
         return env_var
 
