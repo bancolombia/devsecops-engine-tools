5f426d5d1c930a6df837b6db7ece17902e3f6e55
luctoro <luctoro@bancolombia.com.co>
2025-05-05 15:31:27 -0500

    feat(engine_container): Context flag validation

--- tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/usecases/container_sca_scan.py
+++ tools/devsecops_engine_tools/engine_sca/engine_container/src/domain/usecases/container_sca_scan.py
@@ -24,6 +24,7 @@ class ContainerScaScan:
         image_to_scan,
         exclusions,
         pipeline_name,
+        context,
     ):
         self.tool_run = tool_run
         self.remote_config = remote_config
@@ -35,7 +36,8 @@ class ContainerScaScan:
         self.image_to_scan = image_to_scan
         self.exclusions = exclusions
         self.pipeline_name = pipeline_name
-
+        self.context = context
+        
     def get_image(self, image_to_scan):
         """
         Process the list of images.
@@ -137,6 +139,8 @@ class ContainerScaScan:
         Returns:
             list: Deserialized list of findings.
         """
-        self.tool_deseralizator.get_container_context_from_results(image_scanned)
-        
+        context_flag = self.context
+        if context_flag == "true":
+            self.tool_deseralizator.get_container_context_from_results(image_scanned)
+    
         return self.tool_deseralizator.get_list_findings(image_scanned)
