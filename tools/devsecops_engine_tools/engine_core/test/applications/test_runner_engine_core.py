bb3c8d226c043c5272c390a346237696e5082884
luctoro <luctoro@bancolombia.com.co>
2025-05-06 17:30:45 -0500

    test: adding context to tests update

--- tools/devsecops_engine_tools/engine_core/test/applications/test_runner_engine_core.py
+++ tools/devsecops_engine_tools/engine_core/test/applications/test_runner_engine_core.py
@@ -107,6 +107,7 @@ def test_get_inputs_from_cli(mock_parse_args):
     mock_args.xray_mode = "scan"
     mock_args.image_to_scan = "image"
     mock_args.dast_file_path = "dast_file_path"
+    mock_args.context = "false"
 
     # Mock the parse_args method
     mock_parse_args.return_value = mock_args
