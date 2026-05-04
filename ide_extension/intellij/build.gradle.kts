8e3f26c268ab39959c5a89c03ae66b4b56518646
juancgalvis <8420868+juancgalvis@users.noreply.github.com>
2026-05-04 12:30:18 +0000

    fix(deps): update dependencies

--- ide_extension/intellij/build.gradle.kts
+++ ide_extension/intellij/build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
     id("java")
     id("jacoco")
     // IntelliJ Platform Gradle Plugin
-    id("org.jetbrains.intellij.platform") version "2.10.5"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.5.0"
     // Gradle Sonar Plugin
-    id("org.sonarqube") version "7.2.0.6526"
+    id("org.sonarqube") version "7.3.0.8198"
 }
 
 group = properties("pluginGroup").get()
